385c65cde946190d54f406881f11ae092187efb1
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-14 13:34:36 -0700

    Add files via upload

---
+++ event driven programming/List sorting with numbers(ica9)/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace nica9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numUpDown = new System.Windows.Forms.NumericUpDown();
+            this.GENbutton = new System.Windows.Forms.Button();
+            this.SortButton = new System.Windows.Forms.Button();
+            this.STPButton = new System.Windows.Forms.Button();
+            this.DataBox = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Values";
+            // 
+            // numUpDown
+            // 
+            this.numUpDown.Location = new System.Drawing.Point(16, 30);
+            this.numUpDown.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numUpDown.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numUpDown.Name = "numUpDown";
+            this.numUpDown.Size = new System.Drawing.Size(88, 20);
+            this.numUpDown.TabIndex = 1;
+            this.numUpDown.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // GENbutton
+            // 
+            this.GENbutton.Location = new System.Drawing.Point(16, 75);
+            this.GENbutton.Name = "GENbutton";
+            this.GENbutton.Size = new System.Drawing.Size(88, 23);
+            this.GENbutton.TabIndex = 2;
+            this.GENbutton.Text = "Generate";
+            this.GENbutton.UseVisualStyleBackColor = true;
+            this.GENbutton.Click += new System.EventHandler(this.GENbutton_Click);
+            // 
+            // SortButton
+            // 
+            this.SortButton.Location = new System.Drawing.Point(16, 173);
+            this.SortButton.Name = "SortButton";
+            this.SortButton.Size = new System.Drawing.Size(88, 23);
+            this.SortButton.TabIndex = 3;
+            this.SortButton.Text = "Sort";
+            this.SortButton.UseVisualStyleBackColor = true;
+            this.SortButton.Click += new System.EventHandler(this.SortButton_Click);
+            // 
+            // STPButton
+            // 
+            this.STPButton.Location = new System.Drawing.Point(16, 127);
+            this.STPButton.Name = "STPButton";
+            this.STPButton.Size = new System.Drawing.Size(88, 23);
+            this.STPButton.TabIndex = 4;
+            this.STPButton.Text = "Stop";
+            this.STPButton.UseVisualStyleBackColor = true;
+            this.STPButton.Click += new System.EventHandler(this.STPButton_Click);
+            // 
+            // DataBox
+            // 
+            this.DataBox.FormattingEnabled = true;
+            this.DataBox.Location = new System.Drawing.Point(134, 13);
+            this.DataBox.Name = "DataBox";
+            this.DataBox.Size = new System.Drawing.Size(138, 251);
+            this.DataBox.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 270);
+            this.progressBar1.Maximum = 10000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(259, 23);
+            this.progressBar1.Step = 500;
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Value = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 304);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.DataBox);
+            this.Controls.Add(this.STPButton);
+            this.Controls.Add(this.SortButton);
+            this.Controls.Add(this.GENbutton);
+            this.Controls.Add(this.numUpDown);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numUpDown;
+        private System.Windows.Forms.Button GENbutton;
+        private System.Windows.Forms.Button SortButton;
+        private System.Windows.Forms.Button STPButton;
+        private System.Windows.Forms.ListBox DataBox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ event driven programming/List sorting with numbers(ica9)/Form1.cs
@@ -0,0 +1,157 @@
+﻿//*********************************************************************
+//Program:    NICA09 – invoking
+//Author:     Chris Forest
+//Class:      CNT2f
+//Instructor: Kevin More
+//*********************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace nica9
+{
+    //declare the delegate datatype for updating the control
+    delegate void DelVoidInt(int num);
+
+    public partial class Form1 : Form
+    {
+        Thread _thread = null; // reference to the running thread
+        bool _running = true;  // controls stopping the thread loop 
+
+        public Form1()
+        {
+            InitializeComponent();
+            STPButton.Enabled = false; // disable stop button when program loads
+        }
+
+        //********************************************************************************************
+        //Method:  private void cbUpdateProgress(int num)
+        //Purpose: updtae the progress bar for how far in the thred you are
+        //*********************************************************************************************
+        private void cbUpdateProgress(int num)
+        {
+            progressBar1.Value = num;
+        }
+
+        //********************************************************************************************
+        //Method:     private void AddListBox(int num)
+        //Purpose:    add number ti listbox
+        //Parameters: int num- for adding random numbers (between 0-10000)set by a numupdown(between 1000-100000)
+        //*********************************************************************************************
+        private void AddListBox(int num)
+        {
+            DataBox.Items.Add(num);
+        }
+        //********************************************************************************************
+        //Method:  private void SortList()
+        //Purpose: sort all items in list box
+        //*********************************************************************************************
+        private void SortList()
+        {
+            int current = Convert.ToInt32(DataBox.Items.Count); // set current items in list 
+            int temp = Convert.ToInt32(DataBox.Items[0]);       // temp varaible to store list and swap them later on for sorting 
+            int count1, count2;                                 // count varaibles for looping through list
+
+            // loop for hihg numbers
+            for(count1=0;count1< current; count1++)
+            {
+                //loop for lower numbers
+                for(count2=count1+1;count2< current; count2++)
+                {
+                    // if current item is higher that second item swap in acending order
+                    if(Convert.ToInt32(DataBox.Items[count1])> Convert.ToInt32(DataBox.Items[count2]))
+                    {
+                        temp = Convert.ToInt32(DataBox.Items[count1]);
+                        (DataBox.Items[count1]) = Convert.ToInt32(DataBox.Items[count2]);
+                        (DataBox.Items[count2]) = temp;
+                    }
+                }
+            }
+        }
+
+        private void Threading(object obj)
+        {
+            int counter = 0;
+            Random rdm = new Random();
+
+            if (obj is int max)
+            {
+                //the thread loop, loop until Stop pressed or max value
+                while (_running && counter <= max)
+                {
+                    //invoke might throw ObjectDisposedException
+                    try
+                    {
+                        //invoke on the progressbar to update with loop count
+                        DataBox.Invoke(new DelVoidInt(AddListBox), rdm.Next(0,10001));
+                        progressBar1.Invoke(new DelVoidInt(cbUpdateProgress), counter);
+                    }
+                    catch (ThreadStateException ex)
+                    {
+                        MessageBox.Show(ex.Message, "Using Invoke");
+                    }
+
+                    //update the loop count
+                    ++counter;
+
+                    //brief delay to slow down the thread
+                    Thread.Sleep(2);
+                }
+            }
+            //argument was not type int, exit the thread
+            else
+            {
+                MessageBox.Show("Can't run the thread!", "Using Invoke");
+            }
+        }
+
+        //generate button launches the thread and passes in the number of times to loop
+        private void GENbutton_Click(object sender, EventArgs e)
+        {
+            GENbutton.Enabled = false;  // disable generate button
+            SortButton.Enabled = false; // disable sort button
+            STPButton.Enabled = true;   // enable stop button
+
+            //create the thread with a parameter to pass in the int loop count value
+            _thread = new Thread(new ParameterizedThreadStart(Threading));
+
+            //make it a background thread
+            _thread.IsBackground = true;
+
+            //allow the thread loop to run
+            _running = true;
+
+            // update progress bar for thread start
+            progressBar1.Maximum = (int)numUpDown.Value;
+
+            //launch the thread with the int number of times to loop
+            _thread.Start((int)numUpDown.Value);
+            
+        }
+
+        // sort alll current values in list 
+        private void SortButton_Click(object sender, EventArgs e)
+        {
+            SortList();
+        }
+
+        // occurs everytime when pressed
+        // stop the thread and enable generate and sort buttons while disableing stopbutton
+        private void STPButton_Click(object sender, EventArgs e)
+        {
+            _running = false;
+            GENbutton.Enabled = true;
+            SortButton.Enabled = true;
+            STPButton.Enabled = false;
+        }
+
+    }
+}
