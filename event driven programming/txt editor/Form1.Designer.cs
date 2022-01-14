59ea99413c8e88dd80eec78eec4bb03fb81b7301
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-14 13:01:07 -0700

    Add files via upload

---
+++ event driven programming/txt editor/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ICA2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(121, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(596, 426);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(25, 25);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenButton.TabIndex = 1;
+            this.OpenButton.Text = "Open file";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(25, 77);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 2;
+            this.SaveButton.Text = "Save File";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 450);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
+
---
+++ event driven programming/txt editor/Form1.cs
@@ -0,0 +1,121 @@
+﻿//**************************************************************************************************************************
+//Program:    ICA2 - text saving/opening
+//Author:     Chris Forest
+//Class:      CNT2F
+//Instructor: Kevin More
+//**************************************************************************************************************************
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ICA2
+{
+    public partial class Form1 : Form
+    {
+        private bool txtChanged = false; // boolean if file has changed text or not 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // when loaded title is cleared
+        //and when saved file(s) are loaded save name is displayed
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "";// clears form title
+        }
+
+        // when textbox is changed bool varaible changes to true
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            txtChanged = true;
+        }
+
+        //event happens when open file is pressed
+        // if text box has changed or not been saved since last edit
+        // it will ask to save, if not it will prompt the open window
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();                  // creates an open dialog window
+            open.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"; // filters what file to show in dialog box
+            open.FilterIndex = 2;                                        // sets which filter opyin is shown first
+            open.RestoreDirectory = true;                                // directory is restored when the dialog box is closed. 
+            open.FileName = "*.txt";                                     // nmae of file that is opened
+
+            // if textbox was changed since last save prompt to ask for a save dialog box
+            if (txtChanged)
+            {
+                // promppt for result to save or not
+                var result = MessageBox.Show("do you want to save current file?", "save?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                // if yes to save, save file
+                if (result == DialogResult.Yes)
+                {
+                    SaveButton_Click(sender, e);
+                }
+            }
+
+            // if no to save current file prompt to open new file. with error checking
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    StreamReader read = new StreamReader(open.FileName);
+                    this.textBox.Text = read.ReadToEnd();
+                    this.Text = open.SafeFileName;
+                    read.Close();
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                }
+            }
+        }
+
+        // event occurs when save is pressed
+        // checks if file has ".txt" in its name
+        // if not ask to save file
+        // otherwise save automatcially
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();                  // save dialog window can be opemed 
+            save.FileName = "*.txt";                                     // nmae of file that is saved
+            save.RestoreDirectory = true;                                // directory is restored when the dialog box is closed
+            save.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"; // filters what files can be saved as in dialog box
+            save.DefaultExt = "txt";                                     // sets default save flie as 
+
+            //if file doen't have ".txt" save file with name
+            if(!this.Text.Contains("*.txt"))
+            {
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter write = new StreamWriter(save.FileName);
+                    write.Write(this.textBox.Text);
+                    write.Close();
+                }
+            }
+        }
+
+        // event happens when closing in anyway
+        // checks if textbox has text 
+        //ask user if they want to save file before exiting
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(txtChanged)
+            {
+                if (MessageBox.Show("exit without saving?", "exit", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+}
