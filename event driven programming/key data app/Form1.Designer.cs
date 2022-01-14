dd8d65ed74ecf9e9f1a84d07ffecdd2d7d87bc43
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-14 13:12:10 -0700

    Add files via upload

---
+++ event driven programming/key data app/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ica4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.KeyCodeLabel = new System.Windows.Forms.Label();
+            this.CountLabel = new System.Windows.Forms.Label();
+            this.RunLabel = new System.Windows.Forms.Label();
+            this.KeyResult = new System.Windows.Forms.Label();
+            this.onesTotalLabel = new System.Windows.Forms.Label();
+            this.runResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(186, 13);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(227, 303);
+            this.listBox.TabIndex = 0;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // KeyCodeLabel
+            // 
+            this.KeyCodeLabel.AutoSize = true;
+            this.KeyCodeLabel.Location = new System.Drawing.Point(13, 13);
+            this.KeyCodeLabel.Name = "KeyCodeLabel";
+            this.KeyCodeLabel.Size = new System.Drawing.Size(59, 13);
+            this.KeyCodeLabel.TabIndex = 1;
+            this.KeyCodeLabel.Text = "Key Code: ";
+            // 
+            // CountLabel
+            // 
+            this.CountLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.CountLabel.AutoSize = true;
+            this.CountLabel.Location = new System.Drawing.Point(13, 192);
+            this.CountLabel.Name = "CountLabel";
+            this.CountLabel.Size = new System.Drawing.Size(69, 13);
+            this.CountLabel.TabIndex = 2;
+            this.CountLabel.Text = "Ones Count: ";
+            // 
+            // RunLabel
+            // 
+            this.RunLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.RunLabel.AutoSize = true;
+            this.RunLabel.Location = new System.Drawing.Point(13, 225);
+            this.RunLabel.Name = "RunLabel";
+            this.RunLabel.Size = new System.Drawing.Size(71, 13);
+            this.RunLabel.TabIndex = 3;
+            this.RunLabel.Text = "Longest Run:";
+            // 
+            // KeyResult
+            // 
+            this.KeyResult.AutoSize = true;
+            this.KeyResult.Location = new System.Drawing.Point(80, 13);
+            this.KeyResult.Name = "KeyResult";
+            this.KeyResult.Size = new System.Drawing.Size(13, 13);
+            this.KeyResult.TabIndex = 4;
+            this.KeyResult.Text = "0";
+            // 
+            // onesTotalLabel
+            // 
+            this.onesTotalLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.onesTotalLabel.AutoSize = true;
+            this.onesTotalLabel.Location = new System.Drawing.Point(98, 192);
+            this.onesTotalLabel.Name = "onesTotalLabel";
+            this.onesTotalLabel.Size = new System.Drawing.Size(13, 13);
+            this.onesTotalLabel.TabIndex = 5;
+            this.onesTotalLabel.Text = "0";
+            // 
+            // runResultLabel
+            // 
+            this.runResultLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.runResultLabel.AutoSize = true;
+            this.runResultLabel.Location = new System.Drawing.Point(98, 225);
+            this.runResultLabel.Name = "runResultLabel";
+            this.runResultLabel.Size = new System.Drawing.Size(13, 13);
+            this.runResultLabel.TabIndex = 6;
+            this.runResultLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 338);
+            this.Controls.Add(this.runResultLabel);
+            this.Controls.Add(this.onesTotalLabel);
+            this.Controls.Add(this.KeyResult);
+            this.Controls.Add(this.RunLabel);
+            this.Controls.Add(this.CountLabel);
+            this.Controls.Add(this.KeyCodeLabel);
+            this.Controls.Add(this.listBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label KeyCodeLabel;
+        private System.Windows.Forms.Label CountLabel;
+        private System.Windows.Forms.Label RunLabel;
+        private System.Windows.Forms.Label KeyResult;
+        private System.Windows.Forms.Label onesTotalLabel;
+        private System.Windows.Forms.Label runResultLabel;
+    }
+}
+
---
+++ event driven programming/key data app/Form1.cs
@@ -0,0 +1,91 @@
+﻿//*********************************************************************
+//Program:    ica4- lists with binary numbers
+//Author:     Chris Forest
+//Class:      CNT2f
+//Instructor: Kevin More
+//*********************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ica4
+{
+    public partial class Form1 : Form
+    {
+        List<byte> _byteList = new List<byte>(); // create a list of bytes
+        private int _onesTotal = 0;              // count tottal ones with each button input
+        private int _count;                      // loop through lst an bit number accordigly
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //occurs when any key input is occured
+        //show what key input has occured
+        //add key input to list and convert in to a binary number
+        // display total ones from each key input made to onesTotalLabel
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            KeyResult.Text = e.KeyCode.ToString();
+            listBox.Items.Add(Convert.ToString(e.KeyValue, 2).PadLeft(8, '0'));
+            _byteList.Add(Convert.ToByte(e.KeyValue));
+
+            // itterate through displayed binary number counting all the ones in the list
+            for (_count = 0; _count < 8; _count++)
+            {
+                //if 1's are detectred in keyinput add to total ones
+                if ((_byteList[_byteList.Count - 1] & 1 << _count) > 0)
+                    _onesTotal++;
+            }
+            onesTotalLabel.Text = _onesTotal.ToString();
+        }
+
+        //occurs every time a different list item is clicked
+        // check through list item for logest run of ones and store it in runResultLabel
+        // display logest run of on for which ever list item is selected at anytime
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int longestRun = 0; // the longest run of ones in a specific bit
+            int currentRun = 0; // the current highest stored run of ones from an input
+
+            // count longest run for selected index in list
+            try
+            {
+                //itterate through loop checking each byte posistion for a 0 or a 1
+                for (_count = 0; _count < 8; _count++)
+                {
+                    // if byte number is 0 and current number being checked is 1 add to currnt total run of ones
+                    if ((_byteList[listBox.SelectedIndex] & (0x01 << _count)) != 0)
+                    {
+                        // if set bit is one add to currentRun
+                        currentRun++;
+
+                        // update longestRun if new longest run is made
+                        if (longestRun < currentRun)
+                            longestRun = currentRun;
+                    }
+
+                    // if bit is 0 set currentRun to zero
+                    else
+                        currentRun = 0;
+                }
+
+                // display longest ruon to runResultLabel
+                runResultLabel.Text = longestRun.ToString();
+                longestRun = 0;
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show(error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
