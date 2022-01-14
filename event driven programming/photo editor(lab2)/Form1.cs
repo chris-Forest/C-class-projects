2b6e2dc0a34a1245cb055b8ec66ca88cd131422d
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-14 13:39:50 -0700

    Update Form1.cs

--- event driven programming/photo editor(lab2)/Form1.cs
+++ event driven programming/photo editor(lab2)/Form1.cs
@@ -1,8 +1,6 @@
 ﻿//*********************************************************************
 //Program:    Lab 2 – picture editor
 //Author:     Chris Forest
-//Class:      CNT2f
-//Instructor: Kevin More
 //*********************************************************************
 
 using System;
