1aa952afdc2188c70f426051cba878e480691fd1
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-18 16:11:04 -0700

    Add files via upload

---
+++ object oriented programming/using static modifier(ica3)/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Chris_ica03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBarRadius = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRadius)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBarRadius
+            // 
+            this.trackBarRadius.Location = new System.Drawing.Point(24, 12);
+            this.trackBarRadius.Maximum = 100;
+            this.trackBarRadius.Minimum = -100;
+            this.trackBarRadius.Name = "trackBarRadius";
+            this.trackBarRadius.Size = new System.Drawing.Size(485, 45);
+            this.trackBarRadius.TabIndex = 0;
+            this.trackBarRadius.TickFrequency = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 82);
+            this.Controls.Add(this.trackBarRadius);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRadius)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBarRadius;
+    }
+}
+
