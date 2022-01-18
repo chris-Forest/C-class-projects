f5ba7a37be6d288e8c9f3ecd14226ddbfb26ed02
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-18 15:37:32 -0700

    Add files via upload

---
+++ object oriented programming/classes(ica1)/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+//Submission Code : 1201_2300_A01
+// ica01-Chris Forest
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+namespace Chris_ica01
+{
+    public partial class Form1 : Form
+    {
+        Timer _timer = new Timer();
+        public CDrawer _cDrawer;
+        List<TrekLamps> _trekLampsList = new List<TrekLamps>(); // refrence to get class items here in main
+
+        public Form1()
+        {
+            InitializeComponent();
+            _cDrawer = new CDrawer(900, 500,false);
+            _cDrawer.Scale = 50;
+            _cDrawer.BBColour = Color.Black;
+            _timer.Tick += _timer_Tick;
+            _timer.Interval = 100;
+            _timer.Start();
+            KeyDown += Form_KeyDown;
+        }
+
+        private void Form_KeyDown(object sender, KeyEventArgs e)
+        {
+            Random _Randomer = new Random();
+            // call default trek constructor
+            if (e.KeyCode == Keys.F1)
+            {
+                _trekLampsList.Add(new TrekLamps());
+            }
+
+            // call custom constructor: clr orange, 180 threshold,border n/a
+            if (e.KeyCode == Keys.F2)
+            {
+                _trekLampsList.Add(new TrekLamps(180,Color.Orange, 2));
+            }
+
+            // custom constructor with rdm color,rdm #60-220, border 4
+            if (e.KeyCode == Keys.F3)
+            {
+                _trekLampsList.Add(new TrekLamps((byte)_Randomer.Next(20, 221),RandColor.GetColor(), 4 ));
+            }
+
+            //remove the last added treklight
+            if (e.KeyCode == Keys.Escape)
+            {
+                //if have squares-> remove them else do nothing
+                if (_trekLampsList.Count > 0)
+                    _trekLampsList.RemoveAt(_trekLampsList.Count - 1);
+                else
+                    return;
+            }
+        }
+
+        /// <summary>
+        /// render the lights on gdi window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            _cDrawer.Clear();
+            foreach (var item in _trekLampsList)
+            {
+                item.Tick();
+            }
+            for (int i = 0; i < _trekLampsList.Count; i++)
+            {
+                _trekLampsList[i].RenderLamp(_cDrawer, i);
+            }
+            _cDrawer.Render();
+        } 
+    }
+}
---
+++ object oriented programming/classes(ica1)/TrekLamps.cs
@@ -0,0 +1,66 @@
+﻿//Submission Code : 1201_2300_A01
+// ica01-Chris Forest
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+using static System.Diagnostics.Trace;
+
+namespace Chris_ica01
+{
+    class TrekLamps
+    {
+        /// <summary>
+        /// fields neeed for program
+        /// </summary>
+        private Color _LampColor;
+        private int _Border;
+        private byte _byTick;
+        private byte _byToggle;        
+
+        //custom constructor
+        //set fields to custom parameters 
+        public TrekLamps(byte toggle,Color Lamp,int Border = 2)
+        {
+            _Border = Border;
+            _LampColor = Lamp;
+            _byToggle = toggle;
+            Random _Randomer = new Random();
+            _byTick = (byte)_Randomer.Next(0, 256);
+        }
+
+        //default consturctor
+        public TrekLamps() : this(64,RandColor.GetColor(),6)
+        {
+
+        }
+
+        //tick rate for flashing different colors
+        public void Tick()
+        {
+            _byTick += 3;
+        }
+
+        /// <summary>
+        /// render squares on gdi screen
+        /// </summary>
+        /// <param name="cDrawer"></param>
+        /// <param name="lampNum"></param>
+        public void RenderLamp(CDrawer cDrawer,int lampNum)
+        {
+            int row=lampNum % cDrawer.ScaledWidth;
+            int column = lampNum / cDrawer.ScaledWidth;//ScaledHeight
+            if (_byTick>_byToggle)
+                cDrawer.AddRectangle(row, column, 1, 1,_LampColor,_Border,Color.Black);
+            else
+                cDrawer.AddRectangle(row, column, 1, 1, Color.Black, _Border, Color.Black);
+            cDrawer.Render();
+        }
+    }
+}
