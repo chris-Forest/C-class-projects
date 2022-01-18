32e5d4881a84686e496becc9bd9d2db34c33460c
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-18 16:16:07 -0700

    Add files via upload

---
+++ object oriented programming/missile_command(lab2)/Form1.cs
@@ -0,0 +1,238 @@
+﻿//Submission Code : 1201_2300_L02
+//chris Forest
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+
+namespace Chris_lab2
+{
+    public partial class Form1 : Form
+    {
+        List<missile> foemissiles = new List<missile>();     //list to track enemy missiles
+        List<missile> friendlyMissile = new List<missile>(); //list to track friendly missiles
+        CDrawer CDrawer = null;                              //make cDrawaer
+        private int lives = 5;                               //field for user lives
+        private int score = 0;                               //field for users score
+        private double missilsIN = 0;                        //field for enemy incoming missiles 
+        private int gone = 0;                                //
+        private double friendlyIN = 0;                       //field for frienfly incoming missiles
+        private double KD=0.0;                               //field for user k/d ratio
+        enum eGameState { Paused, Unstarted, Over, Running };//enum to track game current state
+        eGameState currentState;                             //enum member for game state
+
+        /// <summary>
+        /// update form title,start timer for gdi interaction
+        /// make events for user interaction
+        /// set inital game state, set up cdrawer
+        /// add text in gdi for prompt to start game
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Missile Comand";
+            timer1.Interval = 100;
+            timer1.Tick += Timer_Tick;
+            timer1.Enabled = false;
+            start.Click += Start_Click;
+            CDrawer = new CDrawer();
+            missile.drawer = CDrawer;
+            currentState = eGameState.Unstarted;
+            CDrawer.AddText("press start to begin", 45, Color.Cyan);
+        }
+
+        /// <summary>
+        /// when pressed game starts
+        /// set new game state and render in missles and other details
+        /// set default lives, 0 score, incoming missiles,explosion radius and timer interval setting
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Start_Click(object sender, EventArgs e)
+        {
+            currentState = eGameState.Running;
+            score = 0;
+            lives = 5;
+            boomTrackBar.Value = 50;
+            missilesUpDown.Value = 5;
+            intervalUpDown.Value = 100;
+            foemissiles.Clear();
+            friendlyMissile.Clear();
+            missile.Loading = true;
+            timer1.Start();
+            timer1.Enabled = true;           
+            missile.Loading = false;           
+        }
+
+        /// <summary>
+        /// the main event for game code
+        /// does everything needed for game to run
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Point pt;           
+            missile.boomRadius = boomTrackBar.Value;
+            labelBommVal.Text = boomTrackBar.Value.ToString();
+
+            //add enemy missiles to list and update label
+            if (foemissiles.Count < missilesUpDown.Value)
+            {
+                missile newMissile = new missile();
+                foemissiles.Add(newMissile);
+                missilsIN += 1;
+            }            
+
+            //colilsion check
+            List<missile> colided = friendlyMissile.Intersect(foemissiles).ToList();
+            
+            //remove all colided missiles
+            //also update score and update counter for removed missiles
+            foreach (missile item in colided)
+            {
+                while (foemissiles.Remove(item))
+                { 
+                    score += 100;
+                    gone+=1;
+                }
+            }            
+
+            //remove any missiles outside of boundaries 
+            friendlyMissile.RemoveAll(missile.Explosion);
+            missile.Loading = true;            
+            foemissiles.RemoveAll(missile.boundaryCheckLeftRIght);
+
+            // if missile reaches bottom of gdi lives --
+            if (foemissiles.RemoveAll(missile.BoundaryCheckUpDown) >= 1)
+            {
+                lives -= 1;
+            }
+
+            // and a "bunker" where frindly misslies spawn
+            CDrawer.AddCenteredRectangle(CDrawer.ScaledWidth / 2, CDrawer.ScaledHeight, 30, 30, Color.Green);
+            CDrawer.AddText($"Lives {lives} Score {score}", 60, Color.Cyan);
+
+            // update labels
+            labelmissIN.Text = missilsIN.ToString();
+            labelDestroyed.Text = gone.ToString();
+            labelFriend.Text = friendlyIN.ToString();
+            labelKD.Text = KD.ToString();
+
+            //update k/d ratio as long as there is at least 1 missle on screen
+            if (friendlyIN>0)
+                KD= (double)Math.Round(Convert.ToDecimal((missilsIN / friendlyIN) ),2);
+           
+            missile.Loading = false;//start rending in missiles and effects
+
+            //movment for enemy missiles
+            foreach (missile item in foemissiles)
+            {
+                item.move();
+                item.DrawMissiles();
+            }
+
+            //movment for friendly missiles
+            foreach (missile item in friendlyMissile)
+            {
+                //bool for friend?
+                item.move();
+                item.DrawMissiles();
+            }
+
+            //on a left click spawn a friendly missile and update label
+            if (CDrawer.GetLastMouseLeftClick(out pt))
+            {
+                friendlyMissile.Add(new missile(pt));
+                friendlyIN++;
+            }
+            
+            //when adjusting timerinterval stop the game
+            //adjust interval and resume game
+            if (timer1.Enabled)
+                timer1.Stop();
+            this.timer1.Interval = (int)intervalUpDown.Value;
+            timer1.Start();
+
+            //if a game over happens
+            //set game over state, stop the timer 
+            //and display game over text
+            if(currentState==eGameState.Over)
+            {
+                currentState = eGameState.Unstarted;
+                timer1.Enabled = false;
+                timer1.Stop();
+                missile.Loading = true;                
+                CDrawer.AddText("GAME OVER", 60, Color.Cyan);
+                missile.Loading = false;
+            }
+
+            //if lives 0 game is over
+            if (lives == 0)
+                currentState = eGameState.Over;           
+        }
+
+
+        /// <summary>
+        /// just reset all of your values including the lists
+        /// and start game again
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void reset_click(object sender, EventArgs e)
+        {
+            score = 0;
+            lives = 5;
+            boomTrackBar.Value = 50;
+            missilesUpDown.Value = 5;
+            intervalUpDown.Value = 100;
+            foemissiles.Clear();
+            friendlyMissile.Clear();
+            missile.Loading = true;
+            CDrawer.AddText("GAME reset ", 60, Color.Cyan);
+            missile.Loading = false;
+        }
+
+        /// <summary>
+        /// pause game and press same button to resume game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pause_click(object sender, EventArgs e)
+        {
+            //if current state is not paused 
+            //update button text and game state
+            //and start/continue the game
+            if (currentState == eGameState.Paused)
+            {
+                pauseBtn.Text = "pause";
+                currentState = eGameState.Running;
+                timer1.Start();
+            }
+
+            //if current state is paused 
+            //update button text, game state
+            //and show text to indicate game is paused             
+            //and pause the game
+            //hide everything currently in gdi window
+            //and re-display it after game is resumed
+            else
+            {
+                pauseBtn.Text = "continue";
+                currentState = eGameState.Paused;
+                timer1.Stop();
+                missile.Loading = true;
+                CDrawer.AddText("Game Paused", 60, Color.Cyan);
+                missile.Loading = false;
+            }
+        }
+    }
+}
---
+++ object oriented programming/missile_command(lab2)/missle.cs
@@ -0,0 +1,236 @@
+﻿//Submission Code : 1201_2300_L02
+//chris Forest
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GDIDrawer;
+
+namespace Chris_lab2
+{
+    class missile
+    {
+        private Point missleStart;                                   //origin or source of the missile
+        private double angle;                                        //firing angle
+        private double length;                                       //missile path length
+        private double Ydestination;                                 //height, altitude
+        private int radius = 5;                                      // inital radius for friendly missiles
+        private int alpha = 255;                                     // to mimic an explsion fade away
+        private int speed = 0;                                       //the speed at which the missile goes at
+        public bool whoIsIt {  get; private set; }                   // true= friendly missile false=enemy missile        
+        private static CDrawer _drawer;                              //static drawer
+        public static CDrawer drawer { set { _drawer = value; } }    
+        private static int _boomRadius;                              //-represents explosion radius
+        public static int boomRadius{ set { _boomRadius = value; } }
+
+        private static Random _rdm;                                 //static randomizer
+        public static bool Loading                                  //bool for rendering and clearing gdi window
+        {
+            set
+            {
+                if (value)
+                    _drawer.Clear();
+                else
+                    _drawer.Render();
+            }
+        }
+
+        /// <summary>
+        /// initialize ranomizer,set explosion radius and init cDrawer
+        /// </summary>
+        static missile()
+        {
+            _rdm = new Random();
+            boomRadius = 50;
+            drawer = null;
+        }
+
+        /// <summary>
+        /// incoming enemy missiles
+        /// </summary>
+        public missile()
+        {
+            double min = 3 * Math.PI / 4;// set min angle
+            double max = 5 * Math.PI / 4;// set max angle
+
+            //set start point for missiles at a random point at top of gdi window
+            missleStart.X = _rdm.Next(_drawer.ScaledWidth);
+            missleStart.Y = 0 + 5 * 2;
+
+            angle = min + (max - min) * _rdm.NextDouble();// set angle between  3pi/4-5pi/4
+            length = 5;
+            speed = 5;
+            //set the enemy missile path toward the bottom of the gdi window
+            Ydestination =_drawer.ScaledHeight;            
+            whoIsIt = false;//set as enemy missile
+        }
+
+        /// <summary>
+        /// friendly missile
+        /// </summary>
+        /// <param name="toClick"></param>
+        public missile(Point toClick )
+        {
+            //start point for friendly missiles(at center point on the bottom edge of gdi window)
+            missleStart.X = _drawer.ScaledWidth/2;
+            missleStart.Y = _drawer.ScaledHeight;  
+            
+            // set y destination to where user would click at in gdi window
+            Ydestination = toClick.Y;
+            //angle based on where users clicks
+            angle = Math.Atan(-1.0*(toClick.X - missleStart.X)/ (toClick.Y - missleStart.Y));
+
+            radius = 10;
+            speed = 20;
+            whoIsIt = true;//set as friendly missile
+        }
+
+        /// <summary>
+        /// calculate the missile location based on its angle
+        /// and current length traveled
+        /// </summary>
+        /// <returns></returns>
+        public Point Where()
+        {
+            //find and set missile location and return it 
+            Point iswhere = new Point();
+            iswhere.X = (int)(Math.Sin(angle) * length) + missleStart.X;
+            iswhere.Y = (int)(-1*Math.Cos(angle) * length) + missleStart.Y;
+            return iswhere;
+        }
+
+
+        /// <summary>
+        /// check if missile is friendly or an enemy 
+        /// then render it
+        /// </summary>
+        public void DrawMissiles()
+        {
+            //foe missile
+            if (whoIsIt == false)
+            {                
+                _drawer.AddCenteredEllipse(Where(), radius * 2, radius * 2, Color.Red);
+                _drawer.AddLine(missleStart.X, missleStart.Y, Where().X, Where().Y, Color.Red);
+            }
+
+            //friendly missile
+            else
+            {            
+                _drawer.AddCenteredEllipse(Where(), radius * 2, radius * 2, Color.FromArgb(alpha,Color.Green));
+                _drawer.AddLine(missleStart.X, missleStart.Y, Where().X, Where().Y, Color.Green);
+            }
+        }
+
+        /// <summary>
+        /// get all missiles to move
+        /// </summary>
+        public void move()
+        {
+            // if foe missile
+            if (whoIsIt==false)
+            {
+                // add current speed to length
+                length += speed;                
+            }
+
+            //If friendly missile
+            else if (whoIsIt==true)
+            {
+               //determine our missile's state/location.
+               if(Where().Y>Ydestination)
+               {
+                   length += speed;
+               }
+
+               //when friendly missile hits enemy missle cause an explosion and fade away explosion
+               else if (Where().Y<=Ydestination)
+               {
+                    speed = 0;
+                    radius += 5;
+
+                    //fade away explosion when it happens
+                    if(radius>=_boomRadius)
+                    {
+                        alpha -= 10;
+                    }
+               }
+            }            
+        }
+
+        /// <summary>
+        /// explosion method to determine if explosion is done or not  
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        public static bool Explosion(missile arg)
+        {
+            //if explosion radius' alpha value is less then or equal to 10
+            //the explosion is done happening
+            if (arg.alpha <= 10)
+                return true;
+            //otherwise explosion is still happening
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// override method for minimum distance for missile colision
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            //if not a missile return false
+            if (!(obj is missile arg))
+                return false;
+
+            //distance check for missile colision, return that distance
+            float dis = (float)Math.Sqrt(Math.Pow(arg.Where().X-Where().X, 2) + Math.Pow(arg.Where().Y - Where().Y, 2));
+            float mindis = this.radius + arg.radius;
+            return (dis <= mindis && arg.whoIsIt != whoIsIt);
+        }
+
+        /// <summary>
+        /// for happy code
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+
+        /// <summary>
+        /// left and right walls boundary check
+        /// if a missile goes past gdi boundaries
+        /// then remove them in main form code
+        /// </summary>
+        /// <param name="foe"></param>
+        /// <returns></returns>
+        public static bool boundaryCheckLeftRIght(missile foe)
+        {
+            //null check
+            if (foe == null)return false;
+
+            // if past gdi walls return its location and have it removed in main 
+            return (foe.Where().X < (0 + foe.radius * 2)) || (foe.Where().X > (_drawer.ScaledWidth + foe.radius * 2));
+        }
+
+        /// <summary>
+        /// top and bottom boundary check
+        /// if a missile goes past gdi boundaries(left and right walls)
+        /// then remove them in main form code
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        public static bool BoundaryCheckUpDown(missile arg)
+        {
+            //null check
+            if (arg == null) return false;
+
+            // if past gdi walls return its location and have it removed in main 
+            return (arg.Where().Y < 0) || (arg.Where().Y > _drawer.ScaledHeight);
+        }     
+    }
+}
