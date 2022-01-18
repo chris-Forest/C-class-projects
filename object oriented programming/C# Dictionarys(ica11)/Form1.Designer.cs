f03b068fdcf27a93e13d131cedd25c2f124c9be5
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-18 15:33:04 -0700

    Add files via upload

---
+++ object oriented programming/C# Dictionarys(ica11)/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace chris_ica11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.Avgbutton = new System.Windows.Forms.Button();
+            this.DGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadButton.Location = new System.Drawing.Point(13, 13);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(237, 23);
+            this.LoadButton.TabIndex = 0;
+            this.LoadButton.Text = "Load File";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            // 
+            // Avgbutton
+            // 
+            this.Avgbutton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Avgbutton.Location = new System.Drawing.Point(13, 43);
+            this.Avgbutton.Name = "Avgbutton";
+            this.Avgbutton.Size = new System.Drawing.Size(237, 23);
+            this.Avgbutton.TabIndex = 1;
+            this.Avgbutton.Text = "Average";
+            this.Avgbutton.UseVisualStyleBackColor = true;
+            // 
+            // DGV
+            // 
+            this.DGV.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV.Location = new System.Drawing.Point(13, 73);
+            this.DGV.Name = "DGV";
+            this.DGV.Size = new System.Drawing.Size(237, 362);
+            this.DGV.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 447);
+            this.Controls.Add(this.DGV);
+            this.Controls.Add(this.Avgbutton);
+            this.Controls.Add(this.LoadButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button Avgbutton;
+        private System.Windows.Forms.DataGridView DGV;
+    }
+}
+
---
+++ object oriented programming/C# Dictionarys(ica11)/Form1.cs
@@ -0,0 +1,149 @@
+﻿//Submission Code : 1201_2300_A11
+//Chris forest
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Diagnostics.Trace; // Pull Trace static helpers local, ie. WriteLine()
+
+namespace chris_ica11
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<byte, int> myDicTable = new Dictionary<byte, int>();
+        private int freqAvg;
+        private BindingSource _bs = new BindingSource(); // Used to wire DATA <> DataGridView
+
+        public Form1()
+        {
+            InitializeComponent();
+            DGV.DataSource = _bs;
+            DGV.RowHeadersVisible = false;
+            DGV.CellFormatting += DGV_CellFormatting;
+            DGV.ColumnHeaderMouseClick += ColumnHeaderClick;
+            LoadButton.Click += LoadButton_Click;
+            Avgbutton.Click += Avgbutton_Click;
+            Text="File-byte-o-tron";
+        }      
+
+        /// <summary>
+        /// opens a file dialog window to a set location(my pictures) 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            byte []pic;
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);//set path to pictures
+
+            //if dialog window ok is pressed
+            //take action
+            if(openFile.ShowDialog()==DialogResult.OK)
+            {
+                //set picture name and get byte data from loaded picture
+                string path = openFile.FileName;
+                pic = File.ReadAllBytes(path);
+
+                //itterate through loaded picture
+                for (int i = 0; i < pic.Length; i++)
+                {
+                    //if dictionary has key of byte, add to dictionary collection
+                    if(myDicTable.ContainsKey(pic[i]))
+                    {
+                        myDicTable[pic[i]]++;
+                    }
+                    //otherwise dictionary is one
+                    else
+                    {
+                        myDicTable[pic[i]] = 1;
+                    }
+                }
+                //then show data in gridview
+                ShowDictionary();
+            }           
+        }
+
+        /// <summary>
+        /// sort by key or value depending on what column us clicked on
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ColumnHeaderClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            //byte ordering(key)
+            if (e.ColumnIndex==0)
+            {               
+                myDicTable= myDicTable.OrderBy(kvp => kvp.Key).ToDictionary(k=>k.Key,v=>v.Value);
+            }
+
+            //byte(key) ordering with freq(val) sort
+            if (e.ColumnIndex == 1)
+            {
+                List<KeyValuePair<byte, int>> kvpList = new List<KeyValuePair<byte, int>>(myDicTable.ToList());
+                kvpList.Sort((b, i) => b.Value.CompareTo(i.Value));
+                myDicTable = kvpList.ToDictionary(k => k.Key, v => v.Value);                
+            }
+            //then show data in gridview
+            ShowDictionary();
+        }
+
+        /// <summary>
+        /// remove all dictionary entries that are less than the current frequency average
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Avgbutton_Click(object sender, EventArgs e)
+        {
+            myDicTable = myDicTable.Where(entry => entry.Value >= freqAvg).ToDictionary(k => k.Key, v => v.Value);
+
+            //if no picture is loaded just retun to prevent crashing
+            if (freqAvg == 0)
+                return;
+
+            //then show data in gridview
+            ShowDictionary();
+        }
+
+        /// <summary>
+        /// format data gridvew cell style accordingly 
+        /// to above or below byte avg
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DGV_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if(e.ColumnIndex==1&&e.Value!=null)
+            {
+                //if cell value is less then the average bytes set background color to coral
+                if ((int)e.Value < freqAvg)
+                {
+                    e.CellStyle.BackColor = Color.Coral;
+                }
+                //else background color is green for above avg bytes
+                else
+                    e.CellStyle.BackColor = Color.Green;
+            }
+        }
+
+        /// <summary>
+        /// after a picture is loaded or avg button is hit or colum header is sorted,
+        /// display data accordingly again with this method
+        /// </summary>
+        private void ShowDictionary()
+        {            
+            freqAvg = (int)myDicTable.Average(x => x.Value);
+            Avgbutton.Text = $"Average : {freqAvg}";//=null; ???first,then???
+            _bs.DataSource = myDicTable.ToList();
+            DGV.Columns[0].HeaderText = "Key";
+            DGV.Columns[1].HeaderText = "Value";
+            DGV.Columns[0].DefaultCellStyle.Format = "X2";
+        }
+    }
+}
