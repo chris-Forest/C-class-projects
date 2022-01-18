91f31d1b472ee5229d872413852a362c306cf4bc
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-18 15:39:06 -0700

    Add files via upload

---
+++ object oriented programming/comparision, predicates and lamdbas(ica7)/Bar.cs
@@ -0,0 +1,125 @@
+﻿//Submission Code : 1201_2300_A07
+//Chris Forest
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+namespace Chris_ica7
+{
+    class Bar : IComparable
+    {
+        public static CDrawer drawer { get; private set; }
+        public static int Height { get; private set; }
+        private static Random _rdm = new Random(1);
+        public int width { get; private set; }
+        public bool Highlight { get; set; }
+        private Color _Color;
+
+        static Bar()
+        {
+            Height = 20;
+            drawer = new CDrawer();
+            drawer.BBColour = Color.White;
+            drawer.ContinuousUpdate = false;
+        }
+
+        public Bar()
+        {
+            // rdm widths 10-190 in increments of 10
+            width = _rdm.Next(1, 20) * 10;
+            Highlight = false;
+            _Color = Color.FromArgb(_rdm.Next(3, 8) * 32 - 1, _rdm.Next(3, 8) * 32, _rdm.Next(3, 8) * 32);
+        }
+
+        //return the width and color if they are the same for both Bars
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Bar arg))
+                return false;
+            return (width == arg.width && _Color == arg._Color);
+        }
+
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+
+        /// <summary>
+        /// display the rectangles 
+        /// and differentiate weither ine is highlighted or not
+        /// </summary>
+        /// <param name="pointer"></param>
+        public void ShowBar(Point pointer)
+        {
+            if (Highlight == true)
+                drawer.AddRectangle(pointer.X,pointer.Y, width, Height, _Color, 2, Color.Yellow);
+            else
+                drawer.AddRectangle(pointer.X, pointer.Y, width, Height, _Color, 1, Color.Black);
+        }
+
+        /// <summary>
+        /// compare by bar colors
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Bar arg))
+                throw new ArgumentException("Bar : IComparable color");
+            return _Color.ToArgb().CompareTo(arg._Color.ToArgb());
+        }
+
+        /// <summary>
+        /// compare by bar width
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int CompareWidth(Bar a,Bar b)
+        {
+            if (a is null && b is null)
+                return 0;
+            else if (a is null )
+                return -1;
+            else if ( b is null)
+                return 1;
+            return a.width.CompareTo(b.width);
+        }
+
+        /// <summary>
+        /// compare by bar color
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int CompareWidthClr(Bar a, Bar b)
+        {
+            if (a is null && b is null)
+                return 0;
+            else if (a is null)
+                return -1;
+            else if (b is null)
+                return 1;
+
+            //if width are the same, sort by color
+            if (a.width == b.width)
+                return a._Color.ToArgb().CompareTo(b._Color.ToArgb());
+            //sort by width
+            else
+                return a.width.CompareTo(b.width);
+        }
+
+        // method to remove bars over a set brightness
+        public static bool Bright(Bar bars)
+        {
+            return bars._Color.GetBrightness() > .6;
+        }
+    }
+}
---
+++ object oriented programming/comparision, predicates and lamdbas(ica7)/Form1.cs
@@ -0,0 +1,165 @@
+﻿//Submission Code : 1201_2300_A07
+//Chris Forest
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chris_ica7
+{
+    public partial class Form1 : Form
+    {
+        List<Bar> bars = new List<Bar>();
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar.Minimum = 10;
+            trackBar.Maximum = 190;
+            trackBar.Scroll += Width_Scroll;
+            PopulateButton.Click += PopulateButton_Click;
+            Clrbutton.Click += Clrbutton_Click;
+            WidthButton.Click += WidthButton_Click;
+            DecButton.Click += DecButton_Click;
+            WidClrButton.Click += WidClrButton_Click;
+            BrightButton.Click += BrightButton_Click;
+            LessButton.Click += LongerButton_Click;
+            MouseMove += _MouseMoves;
+        }
+
+        /// <summary>
+        /// adjust the removel amount size for longer than button
+        /// and display bthe number on the longer than xx button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Width_Scroll(object sender, EventArgs e)
+        {
+            int tb = trackBar.Value / 10 * 10;
+            trackBar.Value = tb;
+            LessButton.Text = $"Longer Than {trackBar.Value}";
+        }
+
+        /// <summary>
+        /// update trackbar and text on longer than button based on trackbar postion
+        /// </summary>
+        private void updateTB()
+        {
+            if(bars.Count>0)
+            {
+                trackBar.Minimum = bars.Min(x => x.width);
+                trackBar.Maximum = bars.Max(x => x.width);
+                trackBar.Value = trackBar.Minimum + ((trackBar.Maximum - trackBar.Minimum) / 2);
+                LessButton.Text = $"Longer Than {trackBar.Value}";
+            }
+        }
+
+        /// <summary>
+        /// diplay bars when promted to
+        /// </summary>
+        private void ShowBars()
+        {
+            Bar.drawer.Clear();
+            Point point = new Point(0, 0);
+            foreach  (Bar item in bars)
+            {
+                // if a bar will not fit on a line 
+                //wrap it to the next line
+                if(point.X+item.width>Bar.drawer.ScaledWidth)
+                {
+                    //wrapping
+                    point.X = 0;
+                    point.Y += Bar.Height;
+                }
+                //show the bars and udate x postion
+                item.ShowBar(point);
+                point.X += item.width;
+            }
+            Bar.drawer.Render(); //render bars in  drawer
+        }
+
+        /// <summary>
+        /// on click display a random set of bars
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PopulateButton_Click(object sender, EventArgs e)
+        {
+            bars.Clear();
+            int rSum =0;//runing sum        
+            int area = Bar.drawer.ScaledWidth * Bar.drawer.ScaledHeight;
+
+            //While your running sum is less than 80% of the ( scaled Area / bar Height )
+            //create a Bar and if it doesn't currently exist in the list add it and update your running sum
+            while (rSum< (0.8*area / Bar.Height ))
+            {
+                Bar bared = new Bar();
+                if (bars.IndexOf(bared) < 0)
+                {
+                    bars.Add(bared);
+                    rSum += bared.width;
+                }
+            }
+            ShowBars();
+            updateTB();
+        }
+
+        //sort by color
+        private void Clrbutton_Click(object sender, EventArgs e)
+        {
+            bars.Sort();
+            ShowBars();
+        }
+
+        //sort by width
+        private void WidthButton_Click(object sender, EventArgs e)
+        {
+            bars.Sort(Bar.CompareWidth);
+            ShowBars();
+        }
+        //sort by width in decending order
+        private void DecButton_Click(object sender, EventArgs e)
+        {
+            //lambda
+            bars.Sort((left, right) => right.width.CompareTo(left.width)); //sort by desc width?
+            ShowBars();
+        }
+
+        //sort by width then color(asending)
+        private void WidClrButton_Click(object sender, EventArgs e)
+        {
+            bars.Sort(Bar.CompareWidthClr);
+            ShowBars();
+        }
+
+        //remove bars based on a set brightness comparsion from a predicate method
+        private void BrightButton_Click(object sender, EventArgs e)
+        {
+            this.Text = $"Removed{bars.RemoveAll(Bar.Bright)} Bars";
+            ShowBars();
+            updateTB();
+        }
+
+        //remove all bars based on the trackbar number from a predicate method
+        //(displayed in button text)
+        private void LongerButton_Click(object sender, EventArgs e)
+        {
+            this.Text = $"Removed{bars.RemoveAll(x=>x.width>trackBar.Value)} Bars";
+            ShowBars();
+            updateTB();
+        }
+
+        //based on mouse position in main ui window 
+        //highlight bars close to that x postion
+        private void _MouseMoves(object sender, MouseEventArgs e)
+        {
+            bars.ForEach(x => x.Highlight = false);
+            bars.FindAll(x => Math.Abs(x.width - e.X) < 10).ForEach(x => x.Highlight = true);
+            ShowBars();
+        }
+    }
+}
