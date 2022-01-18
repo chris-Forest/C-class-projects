97db7921aca89bca186f58ec5335d845f38a48f0
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-18 15:43:25 -0700

    Add files via upload

---
+++ object oriented programming/equals methods(ica4)/Block.cs
@@ -0,0 +1,90 @@
+﻿//Submission Code : 1201_2300_A04
+// Chris Forest
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Diagnostics.Trace;
+using GDIDrawer;
+
+namespace chris_ica04
+{
+    class Block
+    {
+        private static CDrawer drawer;
+        private static Random _rdm = new Random();
+        private static int _size;//member(field)
+        public int size { set { _size = Math.Abs(value); } }//property
+        private Color _Color;
+        private Rectangle rectangle;
+
+        /// <summary>
+        /// load drawings in gdi window
+        /// </summary>
+        public static bool Loading
+        {
+            set
+            {
+                if (value)
+                    drawer.Clear();
+                else
+                    drawer.Render();
+            }
+        }
+
+        //static ctor
+        static Block()
+        {
+            drawer = new CDrawer(_rdm.Next(500, 901), _rdm.Next(300,701), false);
+        }
+
+        //instance ctor
+        public Block(int blockSize)
+        {
+            _Color = RandColor.GetColor();
+            size = blockSize;
+            rectangle.X = _rdm.Next(0, drawer.ScaledWidth - _size);//no overlap??
+            rectangle.Y = _rdm.Next(0, drawer.ScaledHeight - _size);
+            rectangle.Width = _size;
+            rectangle.Height = _size;
+        }
+
+        /// <summary>
+        /// draw the rectangle
+        /// </summary>
+        public void AddBlock()
+        {
+            drawer.AddRectangle(rectangle, _Color);
+        }
+
+        /// <summary>
+        /// prevent blocks form overlapping
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null|| !(obj is Block arg)) return false; // null ? return false
+
+            if (!rectangle.IsEmpty && !arg.rectangle.IsEmpty && rectangle.IntersectsWith(arg.rectangle))
+                return true;
+            else
+                return false;             
+        }
+
+        /// <summary>
+        /// not used, just so program is happy
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return 1;
+        }        
+    }
+}
---
+++ object oriented programming/equals methods(ica4)/Form1.cs
@@ -0,0 +1,86 @@
+﻿//Submission Code : 1201_2300_A04
+// Chris Forest
+
+using GDIDrawer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Diagnostics.Trace;
+
+namespace chris_ica04
+{
+    public partial class Form1 : Form
+    {
+        List<Block> blocks = new List<Block>();
+        int blockSize = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            SizetrackBar.Scroll += Size_Scroll;
+            ADDbutton.Click += ADDbutton_Click;            
+        }
+
+        /// <summary>
+        /// after size is set 
+        /// button will add squares
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ADDbutton_Click(object sender, EventArgs e)
+        {
+            //set tracking ints
+            int counter_blocks = 0;
+            int count_discards = 0;
+
+            // if less than 25 add blocks and les than 1000 discards
+            while (counter_blocks < 25 && count_discards < 1000)
+            {                
+                Block blocky = new Block(blockSize);
+
+                //if not found add blocks
+                if (blocks.IndexOf(blocky) < 0)
+                {
+                    blocks.Add(blocky);
+                    ++counter_blocks;                    
+                }
+
+                // discard
+                else
+                {
+                    ++count_discards;
+                    //track discarded blocks with progressbar 
+                    progressBar.Value = count_discards;
+                }
+
+                Block.Loading = true;
+
+                for (int i = 0; i < blocks.Count; i++)
+                {
+                    blocks[i].AddBlock();
+                }
+                //show my blocks
+                Block.Loading = false;               
+            }
+            //show in title discards and adds(text title) 
+            this.Text = "loaded " + counter_blocks.ToString() + " distinct blocks with " + count_discards.ToString() + " discards";
+        }
+
+        /// <summary>
+        /// adjust block size
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Size_Scroll(object sender, EventArgs e)
+        {
+            labelVal.Text = SizetrackBar.Value.ToString();
+            blockSize = SizetrackBar.Value;
+        }
+    }
+}
