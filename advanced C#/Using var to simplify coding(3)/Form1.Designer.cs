1ceff473417ece00884bc54ddb93fd2f4a3e010b
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-19 12:32:14 -0700

    Add files via upload

---
+++ advanced C#/Using var to simplify coding(3)/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace chris_ica03_var
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OPENbutton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // OPENbutton
+            // 
+            this.OPENbutton.Location = new System.Drawing.Point(13, 13);
+            this.OPENbutton.Name = "OPENbutton";
+            this.OPENbutton.Size = new System.Drawing.Size(75, 23);
+            this.OPENbutton.TabIndex = 0;
+            this.OPENbutton.Text = "open file";
+            this.OPENbutton.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(94, 15);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(279, 360);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 398);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.OPENbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OPENbutton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ advanced C#/Using var to simplify coding(3)/Form1.cs
@@ -0,0 +1,68 @@
+﻿//Submission code 1202_CMPE2800_A03
+//Chris Forest
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace chris_ica03_var
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            OPENbutton.Click += OPENbutton_Click;
+        }
+
+        private void OPENbutton_Click(object sender, EventArgs e)
+        {
+            var open = new OpenFileDialog();
+            open.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"; // filters what file to show in dialog box
+            open.FilterIndex = 2;                                        // sets which filter opyin is shown first
+            open.RestoreDirectory = true;                                // directory is restored when the dialog box is closed. 
+            open.FileName = "*.txt";
+            open.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            
+            if (open.ShowDialog()==DialogResult.OK)
+            {
+                //put contents of file to a string
+                var read = new StreamReader(open.FileName).ReadToEnd().Split('\n','\r').ToList();
+                //remove all whitespace
+                read.RemoveAll(ws => ws.Trim() == "");
+
+                //sum ascii val dictionary keyed by lines from file
+                var dic = read.Distinct().ToDictionary(kk => kk, v => v.Sum(s => s));
+
+                //pull sumed values from previous ToDictionary method as a new list of strings
+                var dic2 = dic.Values.Distinct().ToDictionary(k => k, k => new List<string>());
+                foreach (var item in dic)
+                {
+                    if(dic2.ContainsKey(item.Value))
+                    {
+                        dic2[item.Value].Add(item.Key);
+                    }
+                }
+                //order list by key
+                var sortV = dic2.OrderBy(k => k.Key);
+
+                //display outputs to rich text box
+                richTextBox1.Text += $"Lowest ascii sum: {sortV.First().Key}\n" +
+                    $"lowest string : {sortV.First().Value.Min()}\n" +
+                    $"higest ascii sum: {sortV.Last().Key}\n" +
+                    $"highest string : {sortV.Last().Value.Max()} " +
+                    $"/ {new string(sortV.Last().Value.Max().OrderBy(c=>c).ToArray())}\n" +
+                    $"biggest Collestion count : {sortV.Max(m=>m.Value.Count())} " +
+                    $"- ascii sum : {sortV.Single(mbox=>mbox.Value.Count()==sortV.Max(m => m.Value.Count())).Key}";
+
+            }
+        }
+    }
+}
