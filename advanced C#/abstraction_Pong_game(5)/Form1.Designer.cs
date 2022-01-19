d18ac6b2cfab8db90b50a37707a7d5f98bd28e89
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-19 12:41:31 -0700

    Add files via upload

---
+++ advanced C#/abstraction_Pong_game(5)/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace ChrisF_Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this._rbMouse1 = new System.Windows.Forms.RadioButton();
+            this._rbWASD = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this._rbMouse2 = new System.Windows.Forms.RadioButton();
+            this.ArrowKeys = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(326, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(200, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "player 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(426, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Player 2";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(334, 212);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(90, 40);
+            this.startBtn.TabIndex = 5;
+            this.startBtn.Text = "start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this._rbMouse1);
+            this.groupBox1.Controls.Add(this._rbWASD);
+            this.groupBox1.Location = new System.Drawing.Point(205, 126);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(129, 86);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "controls";
+            // 
+            // _rbMouse1
+            // 
+            this._rbMouse1.AutoSize = true;
+            this._rbMouse1.Location = new System.Drawing.Point(7, 44);
+            this._rbMouse1.Name = "_rbMouse1";
+            this._rbMouse1.Size = new System.Drawing.Size(57, 17);
+            this._rbMouse1.TabIndex = 1;
+            this._rbMouse1.TabStop = true;
+            this._rbMouse1.Text = "Mouse";
+            this._rbMouse1.UseVisualStyleBackColor = true;
+            this._rbMouse1.CheckedChanged += new System.EventHandler(this._rbMouse1_CheckedChanged);
+            // 
+            // _rbWASD
+            // 
+            this._rbWASD.AutoSize = true;
+            this._rbWASD.Location = new System.Drawing.Point(7, 20);
+            this._rbWASD.Name = "_rbWASD";
+            this._rbWASD.Size = new System.Drawing.Size(58, 17);
+            this._rbWASD.TabIndex = 0;
+            this._rbWASD.TabStop = true;
+            this._rbWASD.Text = "WASD";
+            this._rbWASD.UseVisualStyleBackColor = true;
+            this._rbWASD.CheckedChanged += new System.EventHandler(this._rbWASD_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this._rbMouse2);
+            this.groupBox2.Controls.Add(this.ArrowKeys);
+            this.groupBox2.Location = new System.Drawing.Point(431, 126);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(130, 86);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "controls";
+            // 
+            // _rbMouse2
+            // 
+            this._rbMouse2.AutoSize = true;
+            this._rbMouse2.Location = new System.Drawing.Point(7, 44);
+            this._rbMouse2.Name = "_rbMouse2";
+            this._rbMouse2.Size = new System.Drawing.Size(57, 17);
+            this._rbMouse2.TabIndex = 1;
+            this._rbMouse2.TabStop = true;
+            this._rbMouse2.Text = "Mouse";
+            this._rbMouse2.UseVisualStyleBackColor = true;
+            this._rbMouse2.CheckedChanged += new System.EventHandler(this._rbMouse2_CheckedChanged);
+            // 
+            // ArrowKeys
+            // 
+            this.ArrowKeys.AutoSize = true;
+            this.ArrowKeys.Location = new System.Drawing.Point(7, 20);
+            this.ArrowKeys.Name = "ArrowKeys";
+            this.ArrowKeys.Size = new System.Drawing.Size(57, 17);
+            this.ArrowKeys.TabIndex = 0;
+            this.ArrowKeys.TabStop = true;
+            this.ArrowKeys.Text = "Arrows";
+            this.ArrowKeys.UseVisualStyleBackColor = true;
+            this.ArrowKeys.CheckedChanged += new System.EventHandler(this.ArrowKeys_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton _rbMouse1;
+        private System.Windows.Forms.RadioButton _rbWASD;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton _rbMouse2;
+        private System.Windows.Forms.RadioButton ArrowKeys;
+    }
+}
+
---
+++ advanced C#/abstraction_Pong_game(5)/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Abstraction;
+
+namespace ChrisF_Pong
+{
+    public partial class Form1 : Form
+    {
+        Random BallVel = new Random();
+
+        InputAbstraction P1;
+        InputAbstraction P2;        
+
+        private BufferedGraphicsContext _bgc = new BufferedGraphicsContext();
+        private BufferedGraphics _bg = null;
+
+        GraphicsPath ball = new GraphicsPath();
+        GraphicsPath LeftPaddle = new GraphicsPath();
+        GraphicsPath RightPaddle = new GraphicsPath();
+
+        SolidBrush ballColor = new SolidBrush(Color.White);
+        SolidBrush leftPadColor = new SolidBrush(Color.White);
+        SolidBrush rightPadColor = new SolidBrush(Color.White);
+
+        bool playingGame = false;
+
+
+        //Initial xVel and yVel equals to 5
+        int xVel , yVel;
+
+        public Form1()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+            Text = "PONG! - Chris F.";
+
+            //play with this as needed
+            xVel = BallVel.Next(-15, 15);
+            yVel = BallVel.Next(-15, 15);
+
+            //if velocity is zero get a nwe number that isn't zreo
+            //keep going until velocity is not zero
+            while (xVel == 0)
+                xVel = BallVel.Next(-15, 15);
+            while (yVel == 0)
+                yVel = BallVel.Next(-15, 15);
+        }       
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+            label2.Enabled = false;
+            label3.Enabled = false;
+            groupBox1.Visible = false;
+            groupBox2.Visible = false;
+            startBtn.Visible = false;
+            playingGame = true;
+            makegame();
+        }
+       
+        private void pauseGame()
+        {
+            if (P2.Pause == false && P2.Pause == false)
+            {
+                playingGame = true;
+            }
+            else if (P2.Pause == true || P2.Pause == true)
+            {
+                playingGame = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (playingGame)
+            {
+                render();
+                pauseGame();
+            }
+            else
+                pauseGame();
+        }
+
+        private void makegame()
+        {
+            _bg = _bgc.Allocate(CreateGraphics(), ClientRectangle);
+            //play with theese
+            Point leftP = new Point(10, 200);// starting point is missing up top/btm stopage
+            Point rightP = new Point(760, 200);
+            Point ballPos = new Point(380, 230);
+
+            Size paddlesSize = new Size(10, 70);
+            Size ballSize = new Size(10, 10);
+
+            LeftPaddle.AddRectangle(new Rectangle(leftP, paddlesSize));
+            RightPaddle.AddRectangle(new Rectangle(rightP, paddlesSize));
+            ball.AddRectangle(new Rectangle(ballPos, ballSize));
+        }
+
+        private void render()
+        {
+            //Clears the back buffer
+            _bg.Graphics.Clear(Color.Black);
+
+            _bg.Graphics.FillPath(ballColor, ball);
+            _bg.Graphics.FillPath(leftPadColor, LeftPaddle);
+            _bg.Graphics.FillPath(rightPadColor, RightPaddle);
+
+            BallMoves(ball);
+            //player one paddle key input movement
+            if (P1.Up == true)
+            {
+                UPMovePaddle(LeftPaddle);
+
+                if (LeftPaddle.GetBounds().Y < 0)
+                    downMovePaddle(LeftPaddle);
+            }
+
+            if (P1.Down == true)
+            {
+                downMovePaddle(LeftPaddle);
+                if (LeftPaddle.GetBounds().Bottom > Height - 20)
+                    UPMovePaddle(LeftPaddle);
+            }
+            //this can restart game when over// or restart in the middle of playing
+            if (P1.start)
+            {
+                //timer1.Start();
+                //BallMoves(ball);
+                label1.Text = "start game from a key player one";
+            }
+
+            //******************************************Player 2******************************************************
+            //draw shapes and move them based on abs class bools
+            if (P2.Up == true)
+            {
+                UPMovePaddle(RightPaddle);
+                if (RightPaddle.GetBounds().Y < 0)
+                    downMovePaddle(RightPaddle);
+            }
+
+            if (P2.Down == true)
+            {
+                //move paddle down
+                downMovePaddle(RightPaddle);
+                if (RightPaddle.GetBounds().Bottom > Height - 20)
+                    UPMovePaddle(RightPaddle);
+            }
+            //this can restart game when over// or restart in the middle of playing
+            if (P2.start == true)
+            {
+                //timer1.Start();
+                //BallMoves(ball);
+                label1.Text = "start game form p2 left key";
+            }
+
+            _bg.Render();
+        }      
+           
+        #region RBtns
+        private void _rbWASD_CheckedChanged(object sender, EventArgs e)
+        {
+            if(_rbWASD.Checked==true)
+            {
+                _rbMouse1.Enabled = false;
+                P1 = new InputAbstraction(0);
+                KeyDown += P1.press_KeyDown;
+                KeyUp += P1.release_KeyUp;
+            }
+        }
+
+        private void _rbMouse1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(_rbMouse1.Checked==true)
+            {
+                _rbWASD.Enabled = false;
+                P1 = new InputAbstraction(2);
+                MouseMove+= P1.Player_MouseMove;
+            }
+        }
+
+        private void ArrowKeys_CheckedChanged(object sender, EventArgs e)
+        {
+            if(ArrowKeys.Checked==true)
+            {
+                _rbMouse2.Enabled = false;
+                P2 = new InputAbstraction(1);
+                KeyDown += P2.press_KeyDown;
+                KeyUp += P2.release_KeyUp;
+            }
+        }
+
+        private void _rbMouse2_CheckedChanged(object sender, EventArgs e)
+        {
+            if(_rbMouse2.Checked==true)
+            {
+                ArrowKeys.Enabled = false;
+                P2 = new InputAbstraction(2);
+                MouseMove += P2.Player_MouseMove;
+            }
+        }
+        #endregion
+
+        #region movments
+        private void UPMovePaddle(GraphicsPath pad)
+        {
+            Matrix movement = new Matrix();
+
+            movement.Translate(0, -20);
+
+            pad.Transform(movement);
+        }
+
+        private void downMovePaddle(GraphicsPath pad)
+        {
+            Matrix movement = new Matrix();
+
+            movement.Translate(0, 20);
+
+            pad.Transform(movement);
+        }
+        private void BallMoves(GraphicsPath gp)
+        {
+            Matrix ballMove = new Matrix();
+
+            if (gp.GetBounds().Y < 0)
+            {
+                yVel = -yVel;
+            }
+
+            if (gp.GetBounds().X < 0)
+            {
+                xVel = -xVel;
+                //end game
+                timer1.Stop();
+            }
+
+            if (gp.GetBounds().X > ClientRectangle.Width - ball.GetBounds().Width)
+            {
+                xVel = -xVel;
+                //end game
+                timer1.Stop();
+            }
+
+            if (gp.GetBounds().Y > ClientRectangle.Height - ball.GetBounds().Height)
+            {
+                yVel = -yVel;
+            }
+
+            if (ball.GetBounds().IntersectsWith(RightPaddle.GetBounds()))
+            {
+                xVel = -xVel;
+                yVel = -yVel;
+            }
+
+            if (ball.GetBounds().IntersectsWith(LeftPaddle.GetBounds()))
+            {
+                xVel = -xVel;
+                yVel = -yVel;
+            }
+
+            ballMove.Translate(xVel, yVel);
+
+            gp.Transform(ballMove);
+        }
+        #endregion
+    }
+}
