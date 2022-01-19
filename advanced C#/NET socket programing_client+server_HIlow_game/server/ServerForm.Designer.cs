1ab2b384631c5f23c65910bd9760425fef6e5577
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-19 13:01:29 -0700

    Add files via upload

---
+++ advanced C#/NET socket programing_client+server_HIlow_game/server/ServerForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace server
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoSoft = new System.Windows.Forms.RadioButton();
+            this.rdoHard = new System.Windows.Forms.RadioButton();
+            this.DiscoBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clients: 0";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(17, 76);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(583, 511);
+            this.listBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoSoft);
+            this.groupBox1.Controls.Add(this.rdoHard);
+            this.groupBox1.Location = new System.Drawing.Point(129, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(252, 44);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Type disco";
+            // 
+            // rdoSoft
+            // 
+            this.rdoSoft.AutoSize = true;
+            this.rdoSoft.Location = new System.Drawing.Point(97, 21);
+            this.rdoSoft.Name = "rdoSoft";
+            this.rdoSoft.Size = new System.Drawing.Size(42, 17);
+            this.rdoSoft.TabIndex = 1;
+            this.rdoSoft.TabStop = true;
+            this.rdoSoft.Text = "soft";
+            this.rdoSoft.UseVisualStyleBackColor = true;
+            // 
+            // rdoHard
+            // 
+            this.rdoHard.AutoSize = true;
+            this.rdoHard.Location = new System.Drawing.Point(6, 21);
+            this.rdoHard.Name = "rdoHard";
+            this.rdoHard.Size = new System.Drawing.Size(48, 17);
+            this.rdoHard.TabIndex = 0;
+            this.rdoHard.TabStop = true;
+            this.rdoHard.Text = "Hard";
+            this.rdoHard.UseVisualStyleBackColor = true;
+            // 
+            // DiscoBtn
+            // 
+            this.DiscoBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DiscoBtn.Location = new System.Drawing.Point(412, 13);
+            this.DiscoBtn.Name = "DiscoBtn";
+            this.DiscoBtn.Size = new System.Drawing.Size(188, 44);
+            this.DiscoBtn.TabIndex = 3;
+            this.DiscoBtn.Text = "Disconect";
+            this.DiscoBtn.UseVisualStyleBackColor = true;
+            this.DiscoBtn.Click += new System.EventHandler(this.DiscoBtn_Click);
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 599);
+            this.Controls.Add(this.DiscoBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "ServerForm";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoSoft;
+        private System.Windows.Forms.RadioButton rdoHard;
+        private System.Windows.Forms.Button DiscoBtn;
+    }
+}
+
---
+++ advanced C#/NET socket programing_client+server_HIlow_game/server/ServerForm.cs
@@ -0,0 +1,293 @@
+﻿//Submission code : 1202_CMPE2800_HILO_SERVER
+//Chris Forest
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Diagnostics.Trace;
+
+namespace server
+{
+    public partial class ServerForm : Form
+    {
+        Socket _listener = null;
+
+        //every time a client connects make new instance of ClientConnect class and add it to list
+        List<ClientConnect> Listcc = new List<ClientConnect>();//don't know how to use this at all     
+                
+        public ServerForm()
+        {
+            InitializeComponent();
+            Text = "server";
+            Shown += ServerForm_Shown;
+            rdoSoft.Checked = true;
+        }
+
+        /// <summary>
+        /// start a thread for server proccessing
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ServerForm_Shown(object sender, EventArgs e)
+        {
+            StartListen();
+        }
+
+        /// <summary>
+        /// start listening for connections
+        /// and begin accepting connection attepmts
+        /// </summary>
+        void StartListen()
+        {            
+            try
+            {
+                _listener?.Close();
+
+                // Makes the socket, no action yet
+                _listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Wire it up to the network.. mostly likely point of failure.. security
+                _listener.Bind(new IPEndPoint(IPAddress.Any, 1667));
+
+                _listener.Listen(1);
+                
+                Invoke(new Action(() => { listBox1.Items.Insert(0, "Server StartListening"); }));
+
+                //begin accepting connection attempts
+                _listener.BeginAccept(cbAccept, null);
+            }
+            catch (Exception err)
+            {
+                _listener = null;
+                Invoke(new Action(() => { listBox1.Items.Insert(0, $"StartListening:Error {err.Message}"); }));
+            }
+        }
+        
+        /// <summary>
+        /// callback method for accepting a connection
+        /// establish a new client,start a connection
+        /// start recieving data
+        /// display who is connected,how many people are connected
+        /// add connecting clients to a collection
+        /// </summary>
+        /// <param name="ar"></param>
+        void cbAccept(IAsyncResult ar)
+        {
+            try
+            {
+                ClientConnect cc = new ClientConnect();
+
+                //accpet incoming connection requests
+                cc._Client = _listener.EndAccept(ar);
+
+                Invoke(new Action(() => { listBox1.Items.Insert(0, $"{ cc._Client.RemoteEndPoint} :Connected"); }));
+
+                Invoke(new Action(() => { StartRxThread(cc); }));
+
+                //add client socket to collection
+                Listcc.Add(cc);
+
+                Invoke(new Action(() => { label1.Text = $"Clients: {Listcc.Count}"; }));
+                _listener.BeginAccept(cbAccept, null);
+            }
+            catch (Exception exc)
+            {
+                Invoke(new Action(() => { listBox1.Items.Insert(0, $"cbAccept:exception raised : {exc.Message}"); }));
+            }
+        }
+
+        /// <summary>
+        /// start the recieve thread
+        /// </summary>
+        /// <param name="cc"></param>
+        void StartRxThread(ClientConnect cc)
+        {
+            Thread re = new Thread(rx);
+            re.IsBackground = true;
+            re.Start(cc);
+        }
+
+        /// <summary>
+        /// thread method
+        /// detects a hrad and soft disco
+        /// calls the proccess method from connect class
+        /// get and send response from client guess
+        /// </summary>
+        /// <param name="obj"></param>
+        void rx(object obj)
+        {
+            byte[] buff = new byte[2000];
+
+            //if object is not the class return
+            if (!(obj is ClientConnect sock)) return;
+
+            //keeprunning game until a win or disco is detected
+            while (true)
+            {
+                string resp = "";
+                int NumBytes = 0;
+                try
+                {
+                    NumBytes = sock._Client.Receive(buff);
+                }
+                catch (Exception exc)//hard disco detection
+                {
+                    Invoke(new Action(() => { listBox1.Items.Insert(0, $"Rx: Hard disco {exc.Message}"); }));
+                    sock._Client.Close();
+                    Listcc.Remove(sock);
+                    Invoke(new Action(() => { label1.Text = $"Clients: {Listcc.Count}"; }));
+                    return;
+                }
+                //soft disco detection
+                //updates connected clients label 
+                if (NumBytes == 0)
+                {
+                    Invoke(new Action(() => { listBox1.Items.Insert(0, $"Rx: soft disco "); }));
+                    sock._Client.Disconnect(false);
+                    Listcc.Remove(sock);
+                    Invoke(new Action(() => { label1.Text = $"Clients: {Listcc.Count}"; }));
+                    return;
+                }
+                resp = sock.proccess(buff);
+
+                if (sock.won)
+                {            
+                    sock._Client.Disconnect(false);//soft disco
+                    Listcc.Remove(sock);
+                    Invoke(new Action(() => { label1.Text = $"Clients: {Listcc.Count}"; }));
+                }
+
+                Invoke(new Action(() => { listBox1.Items.Insert(0, $"{resp}"); }));
+            }
+        }
+                
+        /// <summary>
+        /// based on rdo btn choice
+        /// hard of soft disco a client on btn click
+        /// remove client from class collection
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DiscoBtn_Click(object sender, EventArgs e)
+        {
+            for (int i = Listcc.Count - 1; i >= 0; i--)
+            {
+                //hard dc by server
+                if (rdoHard.Checked)
+                {
+                    Listcc[i]._Client.Close();                              
+                    Listcc.Remove(Listcc[i]);
+                    Invoke(new Action(() => { label1.Text = $"Clients: {Listcc.Count}"; }));
+                }
+                //soft disco by server
+                if (rdoSoft.Checked)
+                {
+                    Listcc[i]._Client.Disconnect(false);                 
+                    Listcc.Remove(Listcc[i]);
+                    Invoke(new Action(() => { label1.Text = $"Clients: {Listcc.Count}"; }));
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// evey time a new client is connected re-establish(new instance) of client
+    /// new client connected, new random number picked
+    /// </summary>
+    public class ClientConnect
+    {
+        public Socket _Client = null;
+        BinaryFormatter _bf = new BinaryFormatter(); // formatter (how to serialize)
+        private static Random rng = new Random();
+
+        public int Attempts = 0;//trck number of attepts made by clients
+        public int SecretNumber = rng.Next(0, 1001);//get random number from range assigning it to varible
+
+        //bool for win game
+        public bool won { get; private set; }
+
+        /// <summary>
+        /// procces clients guess
+        /// and assgin a string response to be displayed in client form
+        /// </summary>
+        /// <param name="buff"></param>
+        /// <returns></returns>
+        public string proccess(byte[] buff)
+        {
+            string resp = "";
+            object frame = null;
+            Random rng = new Random();
+
+            object obj = _bf.Deserialize(new MemoryStream(buff));
+
+            Attempts++;
+
+            //if object Deserialize is the guess frame
+            //start proccessing clients guess            
+            if (obj is WinFrameLib.GuessFrame gf)
+            {
+                //comparsons of guess to hidden number                    
+                if (gf.Value < SecretNumber)//if guess too low
+                {
+                    WinFrameLib.LowFrame lf = new WinFrameLib.LowFrame();
+                    var ToLow = (WinFrameLib.LowFrame.EHint[])Enum.GetValues(typeof(WinFrameLib.LowFrame.EHint));
+                    lf.hint = ToLow[rng.Next(ToLow.Length)];
+                    frame = lf;
+
+                    resp = $"IP:{_Client.LocalEndPoint} :Number {SecretNumber} :Guess {gf.Value}";
+                }
+                //if guess too high
+                else if (gf.Value > SecretNumber)
+                {
+                    //call high frame with rdm enum hint response
+                    WinFrameLib.HiFrame hf = new WinFrameLib.HiFrame();
+                    var ToHigh = (WinFrameLib.HiFrame.EHint[])Enum.GetValues(typeof(WinFrameLib.HiFrame.EHint));
+                    hf.hint = ToHigh[rng.Next(ToHigh.Length)];
+                    frame = hf;
+
+                    resp = $"IP:{_Client.LocalEndPoint} :Number {SecretNumber} :Guess {gf.Value}";
+                }
+                //win condition
+                else if (gf.Value == SecretNumber)
+                {
+                    won = true;
+                    WinFrameLib.WinFrame wf = new WinFrameLib.WinFrame();
+                    string winMsg = $"won in {Attempts} Attempts";
+                    wf.Congrats = winMsg;
+                    frame = wf;
+
+                    resp = $"{_Client.RemoteEndPoint}: won in {Attempts} Attempts";
+                }              
+
+                try
+                {
+                    MemoryStream ms = new MemoryStream();
+                    _bf.Serialize(ms, frame);
+                    //send back response to client
+                    _Client.Send(ms.GetBuffer(), (int)ms.Length, SocketFlags.None);
+                }
+                catch (Exception exc)
+                {
+                    resp = $"send err: {exc.Message}";
+                }
+            }
+            else//if bad frame detected
+            {
+                resp = "unknown frame recieved";
+                _Client.Disconnect(false);                
+            }
+            return resp;
+        }
+    }
+}
