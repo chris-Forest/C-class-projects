2d17fca7b3361e07deed00ceb31ebaca838fccfe
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-19 13:20:25 -0700

    Add files via upload

---
+++ advanced C#/async_await functions(L2)/molar mass calculator(LINQ)/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Chris_Zeek_MMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGV = new System.Windows.Forms.DataGridView();
+            this.ColumnATMnum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColmName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColmSymbol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColmMass = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NameButton = new System.Windows.Forms.Button();
+            this.SymButton = new System.Windows.Forms.Button();
+            this.AtomicNumButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ChemFormTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MassTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGV
+            // 
+            this.DGV.AllowUserToAddRows = false;
+            this.DGV.AllowUserToDeleteRows = false;
+            this.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnATMnum,
+            this.ColmName,
+            this.ColmSymbol,
+            this.ColmMass});
+            this.DGV.Location = new System.Drawing.Point(13, 13);
+            this.DGV.Name = "DGV";
+            this.DGV.ReadOnly = true;
+            this.DGV.Size = new System.Drawing.Size(611, 385);
+            this.DGV.TabIndex = 0;
+            // 
+            // ColumnATMnum
+            // 
+            this.ColumnATMnum.HeaderText = "Atomic Number";
+            this.ColumnATMnum.Name = "ColumnATMnum";
+            this.ColumnATMnum.ReadOnly = true;
+            this.ColumnATMnum.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
+            // 
+            // ColmName
+            // 
+            this.ColmName.HeaderText = "Name";
+            this.ColmName.Name = "ColmName";
+            this.ColmName.ReadOnly = true;
+            // 
+            // ColmSymbol
+            // 
+            this.ColmSymbol.HeaderText = "Symbol";
+            this.ColmSymbol.Name = "ColmSymbol";
+            this.ColmSymbol.ReadOnly = true;
+            // 
+            // ColmMass
+            // 
+            this.ColmMass.HeaderText = "Mass";
+            this.ColmMass.Name = "ColmMass";
+            this.ColmMass.ReadOnly = true;
+            // 
+            // NameButton
+            // 
+            this.NameButton.Location = new System.Drawing.Point(630, 13);
+            this.NameButton.Name = "NameButton";
+            this.NameButton.Size = new System.Drawing.Size(158, 23);
+            this.NameButton.TabIndex = 1;
+            this.NameButton.Text = "Sort By Name";
+            this.NameButton.UseVisualStyleBackColor = true;
+            // 
+            // SymButton
+            // 
+            this.SymButton.Location = new System.Drawing.Point(631, 43);
+            this.SymButton.Name = "SymButton";
+            this.SymButton.Size = new System.Drawing.Size(157, 23);
+            this.SymButton.TabIndex = 2;
+            this.SymButton.Text = "Single Character Symbols";
+            this.SymButton.UseVisualStyleBackColor = true;
+            // 
+            // AtomicNumButton
+            // 
+            this.AtomicNumButton.Location = new System.Drawing.Point(631, 72);
+            this.AtomicNumButton.Name = "AtomicNumButton";
+            this.AtomicNumButton.Size = new System.Drawing.Size(158, 23);
+            this.AtomicNumButton.TabIndex = 3;
+            this.AtomicNumButton.Text = "Sort By Atomic #";
+            this.AtomicNumButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 418);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Chemical Formula:";
+            // 
+            // ChemFormTextBox
+            // 
+            this.ChemFormTextBox.Location = new System.Drawing.Point(111, 415);
+            this.ChemFormTextBox.Name = "ChemFormTextBox";
+            this.ChemFormTextBox.Size = new System.Drawing.Size(336, 20);
+            this.ChemFormTextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(519, 418);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Aprorox. Molar Mass";
+            // 
+            // MassTextBox
+            // 
+            this.MassTextBox.Location = new System.Drawing.Point(628, 415);
+            this.MassTextBox.Name = "MassTextBox";
+            this.MassTextBox.ReadOnly = true;
+            this.MassTextBox.Size = new System.Drawing.Size(152, 20);
+            this.MassTextBox.TabIndex = 7;
+            this.MassTextBox.Text = "0";
+            this.MassTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MassTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ChemFormTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AtomicNumButton);
+            this.Controls.Add(this.SymButton);
+            this.Controls.Add(this.NameButton);
+            this.Controls.Add(this.DGV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGV;
+        private System.Windows.Forms.Button NameButton;
+        private System.Windows.Forms.Button SymButton;
+        private System.Windows.Forms.Button AtomicNumButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ChemFormTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox MassTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnATMnum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColmName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColmSymbol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColmMass;
+    }
+}
+
---
+++ advanced C#/async_await functions(L2)/molar mass calculator(LINQ)/Form1.cs
@@ -0,0 +1,254 @@
+﻿//1202_CMPE2800_MMC
+//Ezekiel Enns
+//Chris Forest
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+#region project functionlity(notes)
+/* PARTS
+ * datagrid view
+ *      get table as a file, populate grid view
+ *      only req to get elements for 1-99 inclusive(can go above this)     
+ *      results in datagrid view must come from anonmous types made by linq expressions
+ *      
+ * buttons
+ *      sorts grid view
+ *      buttons will clear inputs and ovveride all current outputs
+ *      
+ * isolate in datagridview to show specfic elements being called from chem formula
+ *      2 - outlines
+ *          0-state 
+ *          1-calculation
+ *      
+ * two states to calculator
+ * zero state:
+ *      Atomic number, 
+ *      Name Symbol, 
+ *      Mass
+ *      
+ * calc state:
+ *      Element
+ *      Count
+ *      Mass per 1
+ *      Total mass per element count(count*mass=total mass) 
+ * 
+ * Checmical Search
+ * 
+ *      use of regex class is needed here
+ *      takes formula and parses it (elemets in use and there counts)
+ *      formula is taken and aprox molar mass is calculated
+ *      using color codeing for molar mass textBox display indicating success(green)/failure(red)
+ *    
+ * apply aprox. molar mass formula
+ *      Molar mass= Mass of substance(in grams)/ number of mole of a substance *      
+ *
+ */
+#endregion 
+
+namespace Chris_Zeek_MMC
+{
+    public partial class Form1 : Form
+    {
+        private List<atom> _pTableDic = new List<atom>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            loadConfig();
+            NameButton.Click += SortOhMatic;
+            SymButton.Click += SortOhMatic;
+            AtomicNumButton.Click += SortOhMatic;
+            ChemFormTextBox.TextChanged += ChemFormTextBox_TextChanged;
+        }
+
+        #region events
+        /// <summary>
+        /// runs regex parse everytime input is added,
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ChemFormTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //removing anything that dose not have use
+
+            //creating a dictionary based on regex finds
+            Regex rx = new Regex(@"(?'sym'([A-Z][a-z])|[A-Z])((?'moles'\d+)|(?=[A-Z])|$)");
+            Dictionary<string, int> foundDic = new Dictionary<string, int>();
+            string value = ChemFormTextBox.Text;
+            foreach (Match item in rx.Matches(value))
+            {
+                string sym = item.Groups["sym"].Value;
+                int moles = 0;
+                if (item.Groups["moles"].Length > 0)
+                    moles = int.Parse(item.Groups["moles"].Value);
+                else
+                    moles = 1;
+
+                if (foundDic.ContainsKey(sym))
+                    foundDic[sym] += moles;
+                else
+                    foundDic.Add(sym, moles);
+            }
+
+            //this determins weather everything fit into the regex above
+            value = rx.Replace(value, "");
+
+            //joining found dic with matches from elements to make new dic that contains all info needed for info
+            var Elements = from f in foundDic
+                           join A in _pTableDic on f.Key equals A.Symbol
+                           select new
+                           {
+                               sym = f.Key,
+                               name = A.Name,
+                               count = f.Value,
+                               mass = A.Mass,
+                               tMass = A.Mass * f.Value
+                           };
+
+            DGV.Rows.Clear();
+
+            //when a value is invalid it will turn red and show the user the table
+            if (ChemFormTextBox.Text.Trim().Length == 0 || value.Length != 0 || foundDic.Count() != Elements.Count())
+            {
+                //setting colors
+                ChemFormTextBox.BackColor = Color.Red;
+                //so this is a common glitch that happens, if you just change 
+                //the bkg color of a readonly it allows you to change the for color
+                MassTextBox.BackColor = Color.White;
+                MassTextBox.ForeColor = Color.Gray;
+
+                //if the header is not set we know this needs to happen
+                if (DGV.Columns[0].HeaderText != "AtomicNumber")
+                {
+                    DGV.Columns[0].HeaderText = "AtomicNumber";
+                    DGV.Columns[1].HeaderText = "Name";
+                    DGV.Columns[2].HeaderText = "Symbol";
+                    DGV.Columns[3].HeaderText = "Mass";
+
+                    foreach (atom item in _pTableDic)
+                        DGV.Rows.Add(item.getDGVrows);
+                }
+            }
+
+            //when everything is valid it will work
+            else
+            {
+                //setting colors
+                MassTextBox.ForeColor = Color.Green;
+                MassTextBox.BackColor = Color.White;
+                ChemFormTextBox.BackColor = Color.White;
+
+                //adding the new sorted rows
+                if (DGV.Columns[0].HeaderText != "Elements")
+                {
+                    DGV.Columns[0].HeaderText = "Elements";
+                    DGV.Columns[1].HeaderText = "Count";
+                    DGV.Columns[2].HeaderText = "Mass";
+                    DGV.Columns[3].HeaderText = "TotalMass";
+                }
+
+                //calculating mass
+                double ct = 0;
+                foreach (var item in Elements)
+                {
+                    DGV.Rows.Add(item.name, item.count, item.mass, item.tMass);
+                    ct += item.mass * item.count;
+                }
+                MassTextBox.Text = $"{ct} g/mol";
+            }
+        }
+
+        /// <summary>
+        /// loads a config file into a data grid view 
+        /// and sets data grid view to display it
+        /// </summary>
+        private void SortOhMatic(object sender, EventArgs e)
+        {
+            if (sender is Control arg)
+            {
+                IOrderedEnumerable<atom> sort;
+
+                //since the previous event handlers were just diffrent by this line
+                //it makes more sense to compact this simulare functionality all into one
+                switch (arg.Name)
+                {
+                    case "SymButton":
+                        sort = from row in _pTableDic orderby row.Symbol.Length, row.Symbol select row;
+                        break;
+                    case "AtomicNumButton":
+                        sort = from row in _pTableDic orderby row.aNum select row;
+                        break;
+                    case "NameButton":
+                    default:
+                        sort = from row in _pTableDic orderby row.Name select row;
+                        break;
+                }
+
+                ChemFormTextBox.Clear();
+                DGV.Rows.Clear();
+
+                //adding the new sorted rows, dam i love linq
+                foreach (var item in sort)
+                    DGV.Rows.Add(item.getDGVrows);
+            }
+        }
+        #endregion
+
+        private void loadConfig()
+        {
+            //making sure we dont initalize it twice
+            if (_pTableDic.Count == 0)
+            {
+                //row tracker
+                int i = 0;
+                var data = from column in Properties.Resources.configData.Replace("\r\n", "").Split(',')
+                               //grouping all columns by groups of 4
+                           group column by (i++ / 4) into rows
+                           //only selecting ones who have 4 rows
+                           where rows.Count() == 4
+                           select new atom(Convert.ToInt32(rows.ElementAt(0)), rows.ElementAt(1), rows.ElementAt(2), Convert.ToDouble(rows.ElementAt(3)));
+
+                //converting data set into dic
+                _pTableDic = data.ToList<atom>();
+
+                //making taking all atoms in data set and throwing them in to dgv
+                foreach (atom item in data)
+                {
+                    DGV.Rows.Add(item.getDGVrows);
+                }
+            }
+        }
+    }
+    /// <summary>
+    /// a class that contains all the information for a atom
+    /// </summary>
+    public class atom
+    {
+        public string Name, Symbol;
+        public int aNum;
+        public double Mass;
+
+        //getting Data grid view rows
+        public string[] getDGVrows { get => new string[] { aNum.ToString(), Name, Symbol, Mass.ToString() }; }
+
+        public atom(int anum, string name, string symbol, double mass)
+        {
+
+            aNum = anum;
+            Name = name;
+            Symbol = symbol;
+            Mass = mass;
+        }
+    }
+}
