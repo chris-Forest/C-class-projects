9e9dc58c439c8b3cc71a40c6337e2ac77135881e
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-19 12:59:38 -0700

    Add files via upload

---
+++ advanced C#/NET socket programing_client+server_HIlow_game/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using static System.Diagnostics.Trace;
+
+namespace Chrid_Hi_Low_Game
+{
+    public partial class Form1 : Form
+    {
+        Socket _socket = null;
+        BinaryFormatter _bf = new BinaryFormatter();
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "High/LowNumber Gusser";
+        }
+    }
+}
