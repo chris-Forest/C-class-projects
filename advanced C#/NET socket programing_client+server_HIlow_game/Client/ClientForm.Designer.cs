acd719e51cad2bdd885e67150233b5e6529279a0
Chris Forest <59615103+chris-Forest@users.noreply.github.com>
2022-01-19 13:02:00 -0700

    Add files via upload

---
+++ advanced C#/NET socket programing_client+server_HIlow_game/Client/ClientForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace Client
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.AdressBox = new System.Windows.Forms.TextBox();
+            this.PortTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ConnectionButton = new System.Windows.Forms.Button();
+            this.TrackBarGuess = new System.Windows.Forms.TrackBar();
+            this.L_min = new System.Windows.Forms.Label();
+            this.GuessValLabel = new System.Windows.Forms.Label();
+            this.L_High = new System.Windows.Forms.Label();
+            this.GuessButton = new System.Windows.Forms.Button();
+            this.ListBoxStatus = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.TrackBarGuess)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adress:";
+            // 
+            // AdressBox
+            // 
+            this.AdressBox.Location = new System.Drawing.Point(103, 17);
+            this.AdressBox.Name = "AdressBox";
+            this.AdressBox.Size = new System.Drawing.Size(175, 20);
+            this.AdressBox.TabIndex = 1;
+            // 
+            // PortTextBox
+            // 
+            this.PortTextBox.Location = new System.Drawing.Point(374, 20);
+            this.PortTextBox.Name = "PortTextBox";
+            this.PortTextBox.Size = new System.Drawing.Size(143, 20);
+            this.PortTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(311, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port:";
+            // 
+            // ConnectionButton
+            // 
+            this.ConnectionButton.Location = new System.Drawing.Point(606, 20);
+            this.ConnectionButton.Name = "ConnectionButton";
+            this.ConnectionButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectionButton.TabIndex = 5;
+            this.ConnectionButton.Text = "Connect";
+            this.ConnectionButton.UseVisualStyleBackColor = true;
+            // 
+            // TrackBarGuess
+            // 
+            this.TrackBarGuess.Location = new System.Drawing.Point(18, 117);
+            this.TrackBarGuess.Maximum = 1000;
+            this.TrackBarGuess.Name = "TrackBarGuess";
+            this.TrackBarGuess.Size = new System.Drawing.Size(786, 45);
+            this.TrackBarGuess.TabIndex = 6;
+            this.TrackBarGuess.Value = 500;
+            // 
+            // L_min
+            // 
+            this.L_min.AutoSize = true;
+            this.L_min.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.L_min.Location = new System.Drawing.Point(13, 89);
+            this.L_min.Name = "L_min";
+            this.L_min.Size = new System.Drawing.Size(24, 25);
+            this.L_min.TabIndex = 7;
+            this.L_min.Text = "0";
+            // 
+            // GuessValLabel
+            // 
+            this.GuessValLabel.AutoSize = true;
+            this.GuessValLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GuessValLabel.Location = new System.Drawing.Point(384, 89);
+            this.GuessValLabel.Name = "GuessValLabel";
+            this.GuessValLabel.Size = new System.Drawing.Size(48, 25);
+            this.GuessValLabel.TabIndex = 8;
+            this.GuessValLabel.Text = "500";
+            // 
+            // L_High
+            // 
+            this.L_High.AutoSize = true;
+            this.L_High.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.L_High.Location = new System.Drawing.Point(723, 89);
+            this.L_High.Name = "L_High";
+            this.L_High.Size = new System.Drawing.Size(60, 25);
+            this.L_High.TabIndex = 9;
+            this.L_High.Text = "1000";
+            // 
+            // GuessButton
+            // 
+            this.GuessButton.Location = new System.Drawing.Point(18, 168);
+            this.GuessButton.Name = "GuessButton";
+            this.GuessButton.Size = new System.Drawing.Size(786, 43);
+            this.GuessButton.TabIndex = 10;
+            this.GuessButton.Text = "Guess";
+            this.GuessButton.UseVisualStyleBackColor = true;
+            // 
+            // ListBoxStatus
+            // 
+            this.ListBoxStatus.FormattingEnabled = true;
+            this.ListBoxStatus.Location = new System.Drawing.Point(18, 218);
+            this.ListBoxStatus.Name = "ListBoxStatus";
+            this.ListBoxStatus.Size = new System.Drawing.Size(786, 199);
+            this.ListBoxStatus.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(816, 433);
+            this.Controls.Add(this.ListBoxStatus);
+            this.Controls.Add(this.GuessButton);
+            this.Controls.Add(this.L_High);
+            this.Controls.Add(this.GuessValLabel);
+            this.Controls.Add(this.L_min);
+            this.Controls.Add(this.TrackBarGuess);
+            this.Controls.Add(this.ConnectionButton);
+            this.Controls.Add(this.PortTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AdressBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.TrackBarGuess)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox AdressBox;
+        private System.Windows.Forms.TextBox PortTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ConnectionButton;
+        private System.Windows.Forms.TrackBar TrackBarGuess;
+        private System.Windows.Forms.Label L_min;
+        private System.Windows.Forms.Label GuessValLabel;
+        private System.Windows.Forms.Label L_High;
+        private System.Windows.Forms.Button GuessButton;
+        private System.Windows.Forms.ListBox ListBoxStatus;
+    }
+}
+
---
+++ advanced C#/NET socket programing_client+server_HIlow_game/Client/ClientForm.cs
@@ -0,0 +1,223 @@
+﻿//Submission code : 1202_CMPE2800_HILO_CLIENT
+//Chris Forest
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using static System.Diagnostics.Trace;
+using System;
+using WinFrameLib;
+
+/// <summary>
+/// win won game disconsct from server
+/// </summary>
+namespace Client
+{
+    public partial class ClientForm : Form
+    {
+        Socket _socket = null;
+        BinaryFormatter _bf = new BinaryFormatter();
+        Thread re;
+        bool connection = false;
+        public ClientForm()
+        {
+            InitializeComponent();
+            Text = "High/Low Number Guesser";
+            //AdressBox.Text = "bits.net.nait.ca";
+            AdressBox.Text = "127.0.0.1";
+            PortTextBox.Text = "1667";
+            ConnectionButton.Click += ConnectionButton_Click;
+            TrackBarGuess.Scroll += TrackBarGuess_Scroll;
+            GuessButton.Click += GuessButton_Click;
+            GuessButton.Enabled = false;
+            TrackBarGuess.Enabled = false;
+        }
+
+        //save the trackbar val and send guess to server
+        private void GuessButton_Click(object sender, EventArgs e)
+        {            
+            try
+            {
+                ListBoxStatus.Items.Insert(0, $"Guess: {TrackBarGuess.Value}");
+                GuessFrame gf = new GuessFrame();
+                gf.Value = TrackBarGuess.Value;
+
+                MemoryStream ms = new MemoryStream();
+                _bf.Serialize(ms, gf);
+                _socket.Send(ms.GetBuffer(), (int)ms.Length, SocketFlags.None);
+                Thread.Sleep(200);// pause between sends
+            }
+            catch (Exception err)
+            {
+                ListBoxStatus.Items.Insert(0, $"guess: error {err.Message}");
+            }          
+        }     
+
+        //connect to server and disconect from server
+        //on every new connection reset guess inputs
+        private void ConnectionButton_Click(object sender, EventArgs e)
+        {            
+            if (connection == false)
+            {
+                MakeConnection();
+                GuessButton.Enabled = true;
+                TrackBarGuess.Enabled = true;
+                //on every new connection reset inputs
+                TrackBarGuess.Minimum = 0;
+                TrackBarGuess.Maximum = 1000;
+                TrackBarGuess.Value = 500;
+                GuessValLabel.Text = TrackBarGuess.Value.ToString();
+                L_min.Text = TrackBarGuess.Minimum.ToString();
+                L_High.Text = TrackBarGuess.Maximum.ToString();
+            }
+            else
+            {
+                //close connection
+                disconect();
+            }
+        }
+        void disconect()
+        {
+            //Disable both sending and receiving on this Socket.
+            _socket?.Shutdown(SocketShutdown.Both);
+            // Closes the Socket connection and releases all associated resources.
+            _socket?.Close();
+            re.Abort();//not needed           
+            ConnectionButton.Text = "Connect";
+            connection = false;//we disconected
+            GuessButton.Enabled = false;
+            TrackBarGuess.Enabled = false;            
+        }
+        
+        private void MakeConnection()
+        {
+            //ConnectionButton.Text = "Connecting";
+            try
+            {                
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                //_socket.BeginConnect(host,port callback,state)
+                _socket.BeginConnect(AdressBox.Text,Convert.ToInt32(PortTextBox.Text), cbConnect, null);
+                ConnectionButton.Text = "Connecting";//here?
+                //msg to listbox on good connection
+                ListBoxStatus.Items.Insert(0,"Connection: Success");
+            }
+            catch (Exception err)
+            {
+                //msg to listbox on error with connection
+                ListBoxStatus.Items.Insert(0,$"MakeConnection: error {err.Message}");
+            }
+            ConnectionButton.Text = "Disconnect";
+            //can Disconnect when already connected on btn press
+            connection = true;
+        }
+        void cbConnect(IAsyncResult ar)// Async callback, runs in Begin() threadpool thread
+        {
+            try
+            {
+                //end connection
+                _socket.EndConnect(ar);
+                _socket.NoDelay = true;// no delay on send ever..
+                //send guess here?
+                Invoke(new Action(() => { StartReceiveThread(_socket); }));
+            }
+            catch (Exception err)
+            {
+                //msg err?
+                //Invoke(new Action(() => { ListBoxStatus.Items.Insert(0, $"cbConnect: error {err.Message}"); }));
+                ListBoxStatus.Items.Insert(0,$"cbConnect: error {err.Message}");
+            }
+        }
+        //reset all game elements to default in here
+        //call this method to cbConnect(a callBack method)
+        void StartReceiveThread(Socket socket)
+        {
+            //threading?
+            re = new Thread(RecieveThread);
+            re.IsBackground = true;
+            re.Start(socket);
+        }
+        //send thread?
+        void RecieveThread(object obj)
+        {
+            byte[] buff = new byte[2000];
+
+            //if (!(obj is Socket sock)) return;
+            Socket sock = (Socket)obj;
+            // not specifically required (could access as a member)
+            // ensure that receive calls won't timeout!
+            sock.ReceiveTimeout = 0;
+
+            while(true)//go until connection terminates
+            {
+                int iNumBytes = 0;
+                try
+                {
+                    // block on receive - will throw on hard disco
+                    iNumBytes = sock.Receive(buff);                    
+                }
+                catch (SocketException err)
+                {
+                    // invoke handle loss of connection here then end thread!
+                    Invoke(new Action(() => { ListBoxStatus.Items.Insert(0, $"RecieveThread:Hard Disco {err.Message}"); }));
+                    Invoke(new Action(() => { disconect(); }));
+                    return;
+                }
+                if (iNumBytes == 0)//if won game
+                {
+                    // invoke handle loss of connection here then end thread!
+                    Invoke(new Action(() => { ListBoxStatus.Items.Insert(0, $"RecieveThread: soft disco "); })); 
+                    Invoke(new Action(() => { disconect(); }));//soft disco
+                    return;
+                }
+                object objfr = _bf.Deserialize(new MemoryStream(buff));
+              
+                if(objfr is HiFrame high)
+                {                    
+                    Invoke(new Action(() => { updateForm(TrackBarGuess.Value,"high", $"to high {high.hint}"); }));
+                }               
+                else if (objfr is LowFrame low)//else
+                {                    
+                    Invoke(new Action(() => { updateForm(TrackBarGuess.Value, "low", $"to low {low.hint}"); }));
+                }
+                else if (objfr is WinFrame winer)//else
+                {
+                    Invoke(new Action(() => { ListBoxStatus.Items.Insert(0, $"{winer.Congrats}"); }));
+                }
+                else
+                {
+                    Invoke(new Action(() => { ListBoxStatus.Items.Insert(0, $"RecieveThread : unknown"); }));
+                    Invoke(new Action(() => { disconect(); }));
+                    return;
+                }
+            }
+        }
+        void updateForm(int guess,string text,string hint)
+        {
+            ListBoxStatus.Items.Insert(0,$"guess: {hint}");
+            if(text=="high")
+            {
+                //if lower adjust TB to new max
+                TrackBarGuess.Maximum = guess - 1;
+                L_High.Text = TrackBarGuess.Maximum.ToString();
+            }
+            if (text == "low")
+            {
+                //if lower adjust TB to new min 
+                TrackBarGuess.Minimum = guess + 1;
+                L_min.Text = TrackBarGuess.Minimum.ToString();
+            }
+            TrackBarGuess.Value = (TrackBarGuess.Minimum + TrackBarGuess.Maximum) / 2;
+            GuessValLabel.Text = TrackBarGuess.Value.ToString();
+        }
+        private void TrackBarGuess_Scroll(object sender, EventArgs e)
+        {
+            GuessValLabel.Text = TrackBarGuess.Value.ToString();
+        }
+    }
+}
